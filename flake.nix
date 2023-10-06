ee245f9dcd3b5f3d4367e4b6cdead5dcd8dc786d
Lionello Lunesu <lio+git@lunesu.com>
2023-10-06 13:05:29 +0200

    remove unused Nix files

--- flake.nix
+++ flake.nix
@@ -1,19 +1,16 @@
 {
-  outputs = { self, nixpkgs, flake-utils }: {
-    overlays.default = [ (import ./nix/overlay.nix) ];
-  } //
-  flake-utils.lib.eachDefaultSystem
-    (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = self.overlays.default;
-        };
-      in
-      {
-        devShells.default = import ./shell.nix { pkgs = pkgs; };
-        packages.defang-cli = pkgs.callPackage ./nix/cli.nix { };
-        packages.defang-bin = pkgs.callPackage ./pkgs/defang { };
-      }
-    );
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShells.default = import ./shell.nix { pkgs = pkgs; };
+          packages.defang-cli = pkgs.callPackage ./nix/cli.nix { };
+          packages.defang-bin = pkgs.callPackage ./pkgs/defang { };
+        }
+      );
 }
--- shell.nix
+++ shell.nix
@@ -1,19 +1,13 @@
-{ pkgs ? import ./nix/nixpkgs.nix {
-  overlays = [ (import ./nix/overlay.nix) ];
- } }:
+{ pkgs ? import <nixpkgs> {} }:
 with pkgs;
 mkShell {
   buildInputs = [
-    buf
     git
     gnumake
     gnused # force Linux `sed` everywhere
     go_1_20
-    grpc-tools
     nixfmt
     nodejs-18_x # for Pulumi, must match values in package.json
-    protoc-gen-go
-    protoc-gen-go-grpc
     pulumi-bin
   ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.CoreServices
