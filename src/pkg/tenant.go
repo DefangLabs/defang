00771a6a9df929f67ba6b1a1709907e157914aa0
Lionello Lunesu <lio+git@lunesu.com>
2023-11-06 13:41:01 -0800

    remove unused DEFANG_TENANT

--- src/pkg/tenant.go
+++ src/pkg/tenant.go
@@ -3,16 +3,8 @@ package pkg
 type TenantID string
 
 const (
-	DEFANG_TENANT  TenantID = "defang" // the tenant for our own events
-	DEFAULT_TENANT TenantID = ""       // the default tenant (GitHub user ID)
+	DEFAULT_TENANT TenantID = "" // the default tenant (GitHub user ID)
 )
-
-func (t TenantID) GitHubID() string {
-	if t == DEFANG_TENANT {
-		return "defang-io"
-	}
-	return string(t)
-}
 
 func (t TenantID) String() string {
 	if t == DEFAULT_TENANT {
