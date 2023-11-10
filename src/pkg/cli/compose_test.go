770ca9bc2652665776bb8a4a513d33c715103cc8
Lio李歐 <lionello@users.noreply.github.com>
2023-11-09 17:41:24 -0800

    Upload digest (#22)
    * fix compose unit test
    
    * cli: add -force flag to force rebuilds
    
    * cli: better handling for published ports

--- src/pkg/cli/compose_test.go
+++ src/pkg/cli/compose_test.go
@@ -8,9 +8,11 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/compose-spec/compose-go/types"
 	pb "github.com/defang-io/defang/src/protos/io/defang/v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
@@ -65,16 +67,82 @@ func TestLoadDockerCompose(t *testing.T) {
 	})
 }
 
+func TestConvertPort(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    types.ServicePortConfig
+		expected *pb.Port
+		wantErr  string
+	}{
+		{
+			name:    "No target port xfail",
+			input:   types.ServicePortConfig{},
+			wantErr: "port target must be an integer between 1 and 32767",
+		},
+		{
+			name:     "Undefined mode and protocol, target only",
+			input:    types.ServicePortConfig{Target: 1234},
+			expected: &pb.Port{Target: 1234, Mode: pb.Mode_HOST},
+		},
+		{
+			name:    "Published range xfail",
+			input:   types.ServicePortConfig{Target: 1234, Published: "1111-2222"},
+			wantErr: "port published must be empty or equal to target: 1111-2222",
+		},
+		{
+			name:     "Implied ingress mode, defined protocol, published equals target",
+			input:    types.ServicePortConfig{Mode: "ingress", Protocol: "tcp", Published: "1234", Target: 1234},
+			expected: &pb.Port{Target: 1234, Mode: pb.Mode_HOST, Protocol: pb.Protocol_TCP},
+		},
+		{
+			name:     "Implied ingress mode, udp protocol, published equals target",
+			input:    types.ServicePortConfig{Mode: "ingress", Protocol: "udp", Published: "1234", Target: 1234},
+			expected: &pb.Port{Target: 1234, Mode: pb.Mode_HOST, Protocol: pb.Protocol_UDP},
+		},
+		{
+			name:    "Localhost IP, unsupported mode and protocol xfail",
+			input:   types.ServicePortConfig{Mode: "ingress", HostIP: "127.0.0.1", Protocol: "tcp", Published: "1234", Target: 1234},
+			wantErr: "host_ip is not supported",
+		},
+		{
+			name:     "Ingress mode without host IP, single target",
+			input:    types.ServicePortConfig{Mode: "ingress", Protocol: "tcp", Target: 1234},
+			expected: &pb.Port{Target: 1234, Mode: pb.Mode_INGRESS, Protocol: pb.Protocol_HTTP},
+		},
+		{
+			name:    "Ingress mode without host IP, single target, published range xfail",
+			input:   types.ServicePortConfig{Mode: "ingress", Protocol: "tcp", Target: 1234, Published: "1111-2223"},
+			wantErr: "port published must be empty or equal to target: 1111-2223",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := convertPort(tt.input)
+			if err != nil {
+				if tt.wantErr == "" {
+					t.Errorf("convertPort() unexpected error: %v", err)
+				} else if !strings.Contains(err.Error(), tt.wantErr) {
+					t.Errorf("convertPort() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			}
+			if got.String() != tt.expected.String() {
+				t.Errorf("convertPort() got %v, want %v", got, tt.expected.String())
+			}
+		})
+	}
+}
+
 func TestUploadTarball(t *testing.T) {
 	const path = "/upload/x/"
-	const expectedPath = "/upload/x/sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU="
+	const digest = "sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU="
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "PUT" {
 			t.Errorf("Expected PUT request, got %v", r.Method)
 		}
-		if r.URL.Path != expectedPath {
-			t.Errorf("Expected %v, got %v", expectedPath, r.URL.Path)
+		if !strings.HasPrefix(r.URL.Path, path) {
+			t.Errorf("Expected prefix %v, got %v", path, r.URL.Path)
 		}
 		if r.Header.Get("Content-Type") != "application/gzip" {
 			t.Errorf("Expected Content-Type: application/gzip, got %v", r.Header.Get("Content-Type"))
@@ -83,13 +151,26 @@ func TestUploadTarball(t *testing.T) {
 	}))
 	defer server.Close()
 
-	url, err := uploadTarball(context.TODO(), mockClient{server.URL + path}, &bytes.Buffer{})
-	if err != nil {
-		t.Fatalf("uploadTarball() failed: %v", err)
-	}
-	if url != server.URL+expectedPath {
-		t.Errorf("Expected %v, got %v", server.URL+expectedPath, url)
-	}
+	t.Run("upload with digest", func(t *testing.T) {
+		url, err := uploadTarball(context.TODO(), mockGrpcClient{server.URL + path}, &bytes.Buffer{}, digest)
+		if err != nil {
+			t.Fatalf("uploadTarball() failed: %v", err)
+		}
+		const expectedPath = path + digest
+		if url != server.URL+expectedPath {
+			t.Errorf("Expected %v, got %v", server.URL+expectedPath, url)
+		}
+	})
+
+	t.Run("force upload without digest", func(t *testing.T) {
+		url, err := uploadTarball(context.TODO(), mockGrpcClient{server.URL + path}, &bytes.Buffer{}, "")
+		if err != nil {
+			t.Fatalf("uploadTarball() failed: %v", err)
+		}
+		if url != server.URL+path {
+			t.Errorf("Expected %v, got %v", server.URL+path, url)
+		}
+	})
 }
 
 func TestCreateTarballReader(t *testing.T) {
@@ -146,52 +227,52 @@ func TestCreateTarballReader(t *testing.T) {
 	})
 }
 
-type mockClient struct {
+type mockGrpcClient struct {
 	url string
 }
 
-func (m mockClient) CreateUploadURL(ctx context.Context, req *connect.Request[pb.UploadURLRequest]) (*connect.Response[pb.UploadURLResponse], error) {
+func (m mockGrpcClient) CreateUploadURL(ctx context.Context, req *connect.Request[pb.UploadURLRequest]) (*connect.Response[pb.UploadURLResponse], error) {
 	return connect.NewResponse(&pb.UploadURLResponse{Url: m.url + req.Msg.Digest}), nil
 }
-func (mockClient) GetStatus(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[pb.Status], error) {
+func (mockGrpcClient) GetStatus(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[pb.Status], error) {
 	panic("no impl")
 }
-func (mockClient) GetVersion(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[pb.Version], error) {
+func (mockGrpcClient) GetVersion(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[pb.Version], error) {
 	panic("no impl")
 }
-func (mockClient) Tail(context.Context, *connect.Request[pb.TailRequest]) (*connect.ServerStreamForClient[pb.TailResponse], error) {
+func (mockGrpcClient) Tail(context.Context, *connect.Request[pb.TailRequest]) (*connect.ServerStreamForClient[pb.TailResponse], error) {
 	panic("no impl")
 }
-func (mockClient) Update(context.Context, *connect.Request[pb.Service]) (*connect.Response[pb.ServiceInfo], error) {
+func (mockGrpcClient) Update(context.Context, *connect.Request[pb.Service]) (*connect.Response[pb.ServiceInfo], error) {
 	panic("no impl")
 }
-func (mockClient) Get(context.Context, *connect.Request[pb.ServiceID]) (*connect.Response[pb.ServiceInfo], error) {
+func (mockGrpcClient) Get(context.Context, *connect.Request[pb.ServiceID]) (*connect.Response[pb.ServiceInfo], error) {
 	panic("no impl")
 }
-func (mockClient) Delete(context.Context, *connect.Request[pb.DeleteRequest]) (*connect.Response[pb.DeleteResponse], error) {
+func (mockGrpcClient) Delete(context.Context, *connect.Request[pb.DeleteRequest]) (*connect.Response[pb.DeleteResponse], error) {
 	panic("no impl")
 }
-func (mockClient) Publish(context.Context, *connect.Request[pb.PublishRequest]) (*connect.Response[emptypb.Empty], error) {
+func (mockGrpcClient) Publish(context.Context, *connect.Request[pb.PublishRequest]) (*connect.Response[emptypb.Empty], error) {
 	panic("no impl")
 }
-func (mockClient) Subscribe(context.Context, *connect.Request[pb.SubscribeRequest]) (*connect.ServerStreamForClient[pb.SubscribeResponse], error) {
+func (mockGrpcClient) Subscribe(context.Context, *connect.Request[pb.SubscribeRequest]) (*connect.ServerStreamForClient[pb.SubscribeResponse], error) {
 	panic("no impl")
 }
-func (mockClient) GetServices(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[pb.ListServicesResponse], error) {
+func (mockGrpcClient) GetServices(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[pb.ListServicesResponse], error) {
 	panic("no impl")
 }
-func (mockClient) Token(context.Context, *connect.Request[pb.TokenRequest]) (*connect.Response[pb.TokenResponse], error) {
+func (mockGrpcClient) Token(context.Context, *connect.Request[pb.TokenRequest]) (*connect.Response[pb.TokenResponse], error) {
 	panic("no impl")
 }
-func (mockClient) PutSecret(context.Context, *connect.Request[pb.SecretValue]) (*connect.Response[emptypb.Empty], error) {
+func (mockGrpcClient) PutSecret(context.Context, *connect.Request[pb.SecretValue]) (*connect.Response[emptypb.Empty], error) {
 	panic("no impl")
 }
-func (mockClient) ListSecrets(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[pb.Secrets], error) {
+func (mockGrpcClient) ListSecrets(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[pb.Secrets], error) {
 	panic("no impl")
 }
-func (mockClient) GenerateFiles(context.Context, *connect.Request[pb.GenerateFilesRequest]) (*connect.Response[pb.GenerateFilesResponse], error) {
+func (mockGrpcClient) GenerateFiles(context.Context, *connect.Request[pb.GenerateFilesRequest]) (*connect.Response[pb.GenerateFilesResponse], error) {
 	panic("no impl")
 }
-func (mockClient) RevokeToken(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[emptypb.Empty], error) {
+func (mockGrpcClient) RevokeToken(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[emptypb.Empty], error) {
 	panic("no impl")
 }
