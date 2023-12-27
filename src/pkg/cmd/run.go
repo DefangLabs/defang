127d2e2d49972acbeea8b589bc2d427f2cf3d1a9
Lio李歐 <lionello@users.noreply.github.com>
2023-12-27 09:43:08 -0800

    cli: cloud runner (#11)
    * cli: cloud runner
    
    * cli: docker support
    
    * cli: add cloudformation driver
    
    * cli: remove CDK
    
    * fix: pull logs for docker driver
    
    * fix: use crun- as prefix
    
    * cli: support --env-file
    
    * cli: remove pulumi dep
    
    * cli: add stop command
    
    * cli: create VPC+subnet
    
    * const resouce and output ids
    
    * Add --platform arg
    
    * Grab task info after run
    
    * add default security-group
    
    * Add -vpcid arg
    
    * cli: add init flag
    
    * merge both CLIs
    
    * exlude docker tests from short test runs
    
    * fix CI triggers

---
+++ src/pkg/cmd/run.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func Run(ctx context.Context, region Region, image string, memory uint64, color Color, args []string, env map[string]string, platform, vpcId string) error {
+	driver := createDriver(color, region)
+
+	if err := driver.SetVpcID(vpcId); err != nil { // VPC affects the cloudformation template
+		return err
+	}
+
+	if err := driver.SetUp(ctx, image, memory, platform); err != nil {
+		return err
+	}
+
+	id, err := driver.Run(ctx, env, args...)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Task ID:", *id)
+
+	// Try 10 times to get the public IP address
+	for i := 0; i < 10; i++ {
+		info, err := driver.GetInfo(ctx, id)
+		if err != nil {
+			time.Sleep(time.Second)
+			continue
+		}
+		if info != "" {
+			fmt.Println(info)
+		}
+		break
+	}
+
+	// FIXME: stop task on context cancelation/ctrl-c?
+	return driver.Tail(ctx, id)
+}
---
+++ src/pkg/types/driver.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"context"
+)
+
+const (
+	ProjectName = "crun"
+)
+
+type TaskID *string
+
+type Driver interface {
+	SetUp(ctx context.Context, image string, memory uint64, platform string) error
+	TearDown(ctx context.Context) error
+	Run(ctx context.Context, env map[string]string, args ...string) (TaskID, error)
+	Tail(ctx context.Context, taskID TaskID) error
+	Stop(ctx context.Context, taskID TaskID) error
+	// Exec(ctx context.Context, taskID TaskID, args ...string) error
+	GetInfo(ctx context.Context, taskID TaskID) (string, error) // TODO: make return value a struct
+	SetVpcID(vpcId string) error
+}
