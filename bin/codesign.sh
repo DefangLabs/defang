5735c1434dc21bdf46d726867602419ec62ab4a6
Lionello Lunesu <lio+git@lunesu.com>
2024-01-12 22:22:54 -0800

    enable debugging

--- bin/codesign.sh
+++ bin/codesign.sh
@@ -12,6 +12,8 @@ if [ -z "$MACOS_CERTIFICATE_NAME" ]; then
     exit 2
 fi
 
+[ "$ACTIONS_STEP_DEBUG" = 'true' ] || [ "$DEBUG" = 'true' ] && set -x
+
 # We need to create a temporary keychain to store our certificate and provisioning profile, but only in CI
 if [ -n "$RUNNER_TEMP" ]; then
     # assume MACOS_P12_BASE64, KEYCHAIN_PASSWORD, MACOS_P12_PASSWORD are set in the env
@@ -21,21 +23,21 @@ if [ -n "$RUNNER_TEMP" ]; then
     TMP_KEYCHAIN_PATH=$RUNNER_TEMP/app-signing.keychain-db
 
     # import certificate and provisioning profile from secrets
-    echo $MACOS_P12_BASE64 | base64 --decode > $TMP_CERTIFICATE_PATH
+    echo $MACOS_P12_BASE64 | base64 --decode > "$TMP_CERTIFICATE_PATH"
 
     # We need to create a new keychain, otherwise using the certificate will prompt
     # with a UI dialog asking for the certificate password, which we can't
     # use in a headless CI environment
-    security create-keychain -p "$KEYCHAIN_PASSWORD" $TMP_KEYCHAIN_PATH || true
-    # security set-keychain-settings -lut 21600 $TMP_KEYCHAIN_PATH
-        # security default-keychain -s $TMP_KEYCHAIN_PATH
-    security unlock-keychain -p "$KEYCHAIN_PASSWORD" $TMP_KEYCHAIN_PATH
+    security create-keychain -p "$KEYCHAIN_PASSWORD" "$TMP_KEYCHAIN_PATH" || true
+    # security set-keychain-settings -lut 21600 "$TMP_KEYCHAIN_PATH"
+        # security default-keychain -s "$TMP_KEYCHAIN_PATH"
+    security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$TMP_KEYCHAIN_PATH"
 
     # import certificate to keychain
-    security import $TMP_CERTIFICATE_PATH -P "$MACOS_P12_PASSWORD" -t cert -f pkcs12 -k $TMP_KEYCHAIN_PATH -T /usr/bin/codesign
-    security list-keychain -d user -s $TMP_KEYCHAIN_PATH
+    security import "$TMP_CERTIFICATE_PATH" -P "$MACOS_P12_PASSWORD" -t cert -f pkcs12 -k "$TMP_KEYCHAIN_PATH" -T /usr/bin/codesign
+    security list-keychain -d user -s "$TMP_KEYCHAIN_PATH"
 
-    security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "$KEYCHAIN_PASSWORD" $TMP_KEYCHAIN_PATH
+    security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "$KEYCHAIN_PASSWORD" "$TMP_KEYCHAIN_PATH"
 fi
 
 # We finally codesign our app bundle. Add '--options runtime' for the Hardened runtime option (required for notarization)
