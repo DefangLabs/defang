3eafe43c88d0551eccb61f3066d4f263806ab4e3
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2024-01-25 10:32:19 -0800

    somewhat there

--- samples/python/llama/defang-llama/defang_llama/llm.py
+++ samples/python/llama/defang-llama/defang_llama/llm.py
@@ -1,33 +1,7 @@
 import os
-from llama_index import (
-    SimpleDirectoryReader,
-    VectorStoreIndex,
-    ServiceContext,
-)
-from llama_index.llms import LlamaCPP
-from llama_index.llms.llama_utils import (
-    messages_to_prompt,
-    completion_to_prompt,
-)
+from llama_index.llms import Ollama
 
-model_path = os.environ.get("MODEL_PATH", None)
-
-llm = LlamaCPP(
-    # You can pass in the URL to a GGML model to download it automatically
-    model_url=None,
-    # optionally, you can set the path to a pre-downloaded model instead of model_url
-    model_path=model_path,
-    temperature=0.8,
-    max_new_tokens=500,
-    # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
-    context_window=3900,
-    # kwargs to pass to __call__()
-    generate_kwargs={},
-    # kwargs to pass to __init__()
-    # set to at least 1 to use GPU
-    model_kwargs={"n_gpu_layers": 1},
-    # transform inputs into Llama2 format
-    messages_to_prompt=messages_to_prompt,
-    completion_to_prompt=completion_to_prompt,
-    verbose=True,
+llm = Ollama(
+    model=os.environ.get('MODEL', 'phi'),
+    request_timeout=60,
 )
--- samples/python/llama/defang-llama/run.sh
+++ samples/python/llama/defang-llama/run.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
-echo "Model file"
-echo $MODEL_FILE
+echo "Starting ollama"
+ollama serve &
 
-echo "Model url"
-echo $MODEL_URL
+# Wait for ollama to start
+echo "Waiting for ollama to start"
+i=1
+while [ $i -le 20 ]
+do
+  curl -s -o /dev/null -w "%{http_code}" http://localhost:11434 | grep -q '200' && break
+  sleep 1
+  i=$((i+1))
+done
 
-if [ ! -f "/app/models/${MODEL_FILE}" ]; then
-    curl -L $MODEL_URL -o "/app/models/${MODEL_FILE}"
+if [ $i -eq 21 ]
+then
+  echo "Ollama did not start within 20 seconds"
+  exit 1
 fi
+echo "Ollama started"
 
+echo "Pulling model $MODEL"
+ollama pull $MODEL
+
+echo "Starting flask"
 poetry run flask run
