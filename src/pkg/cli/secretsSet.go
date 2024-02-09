e67cc07352a6540f99ecb7b89d4b88b319cc0eab
Lionello Lunesu <lio+git@lunesu.com>
2024-02-09 13:53:31 -0800

    catch empty secret values

--- src/pkg/cli/secretsSet.go
+++ src/pkg/cli/secretsSet.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"context"
+	"errors"
 
 	"github.com/defang-io/defang/src/pkg/cli/client"
 	v1 "github.com/defang-io/defang/src/protos/io/defang/v1"
@@ -9,6 +10,10 @@ import (
 
 func SecretsSet(ctx context.Context, client client.Client, name string, value string) error {
 	Debug(" - Setting secret", name)
+
+	if value == "" {
+		return errors.New("value cannot be empty")
+	}
 
 	if DoDryRun {
 		return nil
