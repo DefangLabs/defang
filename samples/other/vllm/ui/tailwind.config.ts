9ce130161a18d7282fb7937a904132f1a493dbf9
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2024-02-22 20:34:02 -0800

    better ui

--- samples/other/vllm/ui/tailwind.config.ts
+++ samples/other/vllm/ui/tailwind.config.ts
@@ -12,9 +12,12 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-defang":
+          "linear-gradient(311deg, rgba(63, 178, 175, .67), rgba(80, 54, 163, .67) 53%, rgba(9, 23, 76, .85)), linear-gradient(54deg, rgba(255, 131, 122, .25), rgba(255, 131, 122, 0) 28%), linear-gradient(241deg, rgba(228, 122, 255, .32), #d4f0f8 36%)",
+
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 export default config;
