3bd72c728652679c53490f617be73b763719c3ac
Lionello Lunesu <lio+git@lunesu.com>
2024-02-27 11:23:17 -0800

    cli: add debug print

--- src/pkg/cli/bootstrap.go
+++ src/pkg/cli/bootstrap.go
@@ -9,12 +9,14 @@ import (
 )
 
 func BootstrapCommand(ctx context.Context, client client.Client, command string) error {
+	Debug(" - Running CD command", command)
 	if DoDryRun {
 		return errors.New("dry run")
 	}
+	since := time.Now()
 	etag, err := client.BootstrapCommand(ctx, command)
 	if err != nil || etag == "" {
 		return err
 	}
-	return Tail(ctx, client, "", etag, time.Now(), false)
+	return Tail(ctx, client, "", etag, since, false)
 }
