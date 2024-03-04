2e324bcabf7a644fc4e51839232da2d8207c1482
Lio李歐 <lionello@users.noreply.github.com>
2024-03-04 09:52:56 -0800

    tail: only show logs from my cd (#133)
    * tail: only show logs from my cd
    
    * rename LogGroup to LogGroupARN

--- src/pkg/aws/ecs/tail_test.go
+++ src/pkg/aws/ecs/tail_test.go
@@ -7,7 +7,7 @@ import (
 func TestGetLogStreamForTaskID(t *testing.T) {
 	expectedLogStream := "crun/main/12345678123412341234123456789012"
 
-	logStream := getLogStreamForTaskID("12345678123412341234123456789012")
+	logStream := GetLogStreamForTaskID("12345678123412341234123456789012")
 
 	if logStream != expectedLogStream {
 		t.Errorf("Expected log stream %q, but got %q", expectedLogStream, logStream)
