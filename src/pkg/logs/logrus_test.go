f4354c3bd6fa5334aaa6a21237b1d72cd25f5160
Lionello Lunesu <lio+git@lunesu.com>
2024-03-04 15:35:12 -0800

    cli: hide kaniko warnings
    Fixes #146

--- src/pkg/logs/logrus_test.go
+++ src/pkg/logs/logrus_test.go
@@ -13,13 +13,13 @@ func TestIsKanikoError(t *testing.T) {
 		{"info", "INFO[0001] Retrieving image manifest alpine:latest", false},
 		{"trace", "TRAC[0001] blah", false},
 		{"debug", "DEBU[0001] blah", false},
-		{"warn", "WARN[0001] Failed to retrieve image library/alpine:latest", true},
+		{"warn", "WARN[0001] Failed to retrieve image library/alpine:latest", false},
 		{"error", "ERRO[0001] some err", true},
 		{"fatal", "FATA[0001] some err", true},
 		{"panic", "PANI[0001] some err", true},
 		{"trace long", "TRACE long trace message", false},
 		{"ansi info", "\033[36mINFO\033[0m[0001] colored blue", false},
-		{"ansi warn", "\033[33mWARN\033[0m[0001] colored yellow", true},
+		{"ansi warn", "\033[33mWARN\033[0m[0001] colored yellow", false},
 		{"ansi err", "\033[31mERRO\033[0m[0001] colored red", true},
 	}
 	for _, tt := range tests {
