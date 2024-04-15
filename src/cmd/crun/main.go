ec8dc025ce46fabe920a69d4844f0ef3b3b0b51f
Edward J <edw@defang.io>
2024-04-15 13:55:01 -0700

    Create new run command flag set, simplify code

--- src/cmd/crun/main.go
+++ src/cmd/crun/main.go
@@ -11,15 +11,16 @@ import (
 )
 
 var (
-	help     = pflag.BoolP("help", "h", false, "Show this help message")
-	color    = pflag.String("color", "auto", `Colorize output. Choices are: always, never, raw, auto`)
-	envs     = pflag.StringArrayP("env", "e", nil, "Environment variables to pass to the run command")
-	region   = pflag.StringP("region", "r", os.Getenv("AWS_REGION"), "Which cloud region to use, or blank for local Docker")
-	memory   = pflag.StringP("memory", "m", "2g", "Memory limit in bytes")
-	envFiles = pflag.StringArray("env-file", nil, "Read in a file of environment variables")
-	platform = pflag.String("platform", "", "Set platform if host is multi-platform capable")
-	vpcid    = pflag.String("vpcid", "", "VPC to use for the task")
-	subnetid = pflag.String("subnetid", "", "Subnet to use for the task")
+	help   = pflag.BoolP("help", "h", false, "Show this help message")
+	region = pflag.StringP("region", "r", os.Getenv("AWS_REGION"), "Which cloud region to use, or blank for local Docker")
+
+	runFlags = pflag.NewFlagSet(os.Args[0]+" run", pflag.ExitOnError)
+	envs     = runFlags.StringArrayP("env", "e", nil, "Environment variables to pass to the run command")
+	memory   = runFlags.StringP("memory", "m", "2g", "Memory limit in bytes")
+	envFiles = runFlags.StringArray("env-file", nil, "Read in a file of environment variables")
+	platform = runFlags.String("platform", "", "Set platform if host is multi-platform capable")
+	vpcid    = runFlags.String("vpcid", "", "VPC to use for the task")
+	subnetid = runFlags.String("subnetid", "", "Subnet to use for the task")
 	// driver = pflag.StringP("driver", "d", "auto", "Container runner to use. Choices are: pulumi-ecs, docker")
 
 	version = "development" // overwritten by build script -ldflags "-X main.version=..."
@@ -27,7 +28,8 @@ var (
 
 func usage() {
 	fmt.Printf("Cloud runner (%s)\n\n", version)
-	pflag.Usage()
+	fmt.Printf("Usage: \n  %s [command] [options]\n\nGlobal Flags:\n", os.Args[0])
+	pflag.PrintDefaults()
 	fmt.Println(`
 Commands:
   run <image> [arg...]   Create and run a new task from an image
@@ -35,10 +37,14 @@ Commands:
   stop <task ID>         Stop a running task
   info <task ID>         Show information about a task
   destroy                Destroy all resources created by this tool`)
+	fmt.Printf("\n\nUsage of run subcommand: \n  %s run [options]\n\nFlags:\n", os.Args[0])
+	runFlags.PrintDefaults()
 }
 
 func main() {
+	pflag.Usage = usage
 	pflag.Parse()
+
 	if *help {
 		usage()
 		return
@@ -62,21 +68,18 @@ func main() {
 
 	ctx := context.Background()
 
-	requireNoFlags := func() {
-		if len(envMap) > 0 || *platform != "" || *vpcid != "" || *subnetid != "" {
-			cmd.Fatal("stop does not take --env, --env-file, --platform, --vpcid, or --subnetid")
-		}
-	}
-	requireTaskIDForCmd := func(command string) {
+	command := pflag.Arg(0)
+	requireTaskID := func() string {
 		if pflag.NArg() != 2 {
-			cmd.Fatal(command + " requires a single task ID")
+			cmd.Fatal(command + " requires a single task ID argument")
 		}
+		return pflag.Arg(1)
 	}
 
 	var err error
-	switch pflag.Arg(0) {
+	switch command {
 	default:
-		err = errors.New("unknown command: " + pflag.Arg(0))
+		err = errors.New("unknown command: " + command)
 	case "help", "":
 		usage()
 	case "run", "r":
@@ -95,25 +98,18 @@ func main() {
 			SubnetID: *subnetid,
 		})
 	case "stop", "s":
-		requireNoFlags()
-		requireTaskIDForCmd("stop")
-		taskID := pflag.Arg(1)
+		taskID := requireTaskID()
 		err = cmd.Stop(ctx, region, &taskID)
 	case "logs", "tail", "l":
-		requireNoFlags()
-		requireTaskIDForCmd("logs")
-		taskID := pflag.Arg(1)
+		taskID := requireTaskID()
 		err = cmd.Logs(ctx, region, &taskID)
 	case "destroy", "teardown", "d":
-		requireNoFlags()
 		if pflag.NArg() != 1 {
 			cmd.Fatal("destroy does not take any arguments")
 		}
 		err = cmd.Destroy(ctx, region)
 	case "info", "i":
-		requireNoFlags()
-		requireTaskIDForCmd("info")
-		taskID := pflag.Arg(1)
+		taskID := requireTaskID()
 		err = cmd.Info(ctx, region, &taskID)
 	}
 
