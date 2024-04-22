6cf6ede67728cd5309a133e7985646ea4276d5ed
Eric Liu <eric.liu@defang.io>
2024-04-22 13:01:07 -0700

    Update GitHub Action for creating cli wrapper for npm publish

--- packaging/npm/src/cli.ts
+++ packaging/npm/src/cli.ts
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
 
-import * as child_process from 'child_process';
+import * as path from "path";
+import * as child_process from "child_process";
 
-const EXECUTABLE = 'defang';
+const EXECUTABLE = "defang";
 
 function getPathToExecutable(): string {
   let arch = process.arch.toString();
-  let os = process.platform.toString(); 
+  let os = process.platform.toString();
 
-  let extension = '';
-  if (['win32', 'cygwin'].includes(process.platform)) {
-    os = 'windows';
-    extension = '.exe';
+  let extension = "";
+  if (["win32", "cygwin"].includes(process.platform)) {
+    os = "windows";
+    extension = ".exe";
   }
 
-  const executablePath = `${EXECUTABLE}${extension}`;
+  const executablePath = path.join(__dirname, `${EXECUTABLE}${extension}`);
+
   try {
+    console.log(`Looking for ${executablePath}`);
     return require.resolve(executablePath);
   } catch (e) {
     throw new Error(`Could not find application binary at ${executablePath}.`);
@@ -24,7 +27,9 @@ function getPathToExecutable(): string {
 
 function run(): void {
   const args = process.argv.slice(2);
-  const processResult = child_process.spawnSync(getPathToExecutable(), args, { stdio: 'inherit' });
+  const processResult = child_process.spawnSync(getPathToExecutable(), args, {
+    stdio: "inherit",
+  });
   process.exit(processResult.status ?? 0);
 }
 
--- packaging/npm/src/installer.ts
+++ packaging/npm/src/installer.ts
@@ -1,139 +1,165 @@
-import * as fs from 'fs';
-import * as os from 'os';
-import * as path from 'path';
-import * as tar from 'tar';
-import * as http from 'http';
-import * as AdmZip from 'adm-zip';
+import * as fs from "graceful-fs";
+import * as os from "os";
+import * as path from "path";
+import * as tar from "tar";
+import * as AdmZip from "adm-zip";
+import axios from "axios";
 
-type cbErrorHandler = (err?: Error | null) => void | null;
-let platform = os.platform().toString();
-let arch = os.arch().toString();
+async function downloadAppArchive(
+  version: string,
+  archiveFilename: string,
+  outputPath: string
+): Promise<string> {
+  const repo = "defang-io/defang";
+  const downloadUrl = `https://github.com/${repo}/releases/download/v${version}/${archiveFilename}`;
+  const downloadTargetFile = path.join(outputPath, archiveFilename);
 
-function downloadFile(version: string, filename: string, outputPath: string): string {
-  const repo = 'defang-io/defang';
-  const downloadUrl = `https://api.github.com/repos/${repo}/releases/tags/v${version}/${filename}`;
-
-  const downloadTargetFile = path.join(outputPath, filename);
-  const writeFileStream = fs.createWriteStream(downloadTargetFile);
-
-  http.get(downloadUrl, (response) => {
-    response.pipe(writeFileStream);
-    writeFileStream.on('finish', () => {
-      writeFileStream.close();
-      console.log(`File downloaded to ${downloadTargetFile}`);
-      return downloadTargetFile;
-    });
-  }).on('error', (err) => {
-    fs.unlink(downloadTargetFile, () => {
-      console.error(err);
-    });
-  });
-
-  return "";
+  return downloadFile(downloadUrl, downloadTargetFile);
 }
 
-function extractArchive(archiveFilePath: string, outputPath: string, cb: cbErrorHandler): boolean {
-  let result = false;
-  const ext = path.extname(archiveFilePath).toLocaleLowerCase();
-  switch (ext) {
-    case '.zip':
-      result = extractZip(archiveFilePath, outputPath, cb);
-      break;
-    case '.tar.gz':
-      result = extractTarGz(archiveFilePath, outputPath, cb);
-      break;
-    default:
-      cb(new Error(`Unsupported archive extension: ${ext}`));
-  }
+async function downloadFile(
+  downloadUrl: string,
+  downloadTargetFile: string
+): Promise<string> {
+  try {
+    const writeFileStream = fs.createWriteStream(downloadTargetFile);
 
-  return result;
-}
-function extractZip(zipPath: string, outputPath: string, cb: cbErrorHandler): boolean {
-    let result = true;
-    try {
-        let zip = new AdmZip(zipPath);
-        zip.extractAllTo(outputPath, true);
-        cb?.();
-    } catch (error) {
-      result = false;
-      cb(new Error(`An error occurred during ZIP extraction: ${error}`));
+    console.log(`Downloading ${downloadUrl}`);
+    const response = await axios.get(downloadUrl, {
+      responseType: "arraybuffer",
+      headers: {
+        "Content-Type": "application/octet-stream",
+      },
+    });
+
+    if (response?.data === undefined) {
+      throw new Error(
+        `Failed to download ${downloadUrl}. No data in response.`
+      );
     }
 
-    return result;
+    fs.writeFileSync(downloadTargetFile, response.data);
+    return downloadTargetFile;
+  } catch (error) {
+    console.error(`Failed to download ${downloadUrl}. ${error}`);
+    fs.unlinkSync(downloadTargetFile);
+    return "";
+  }
 }
 
-function extractTarGz(tarGzFilePath: string, outputPath: string, cb: cbErrorHandler): boolean  {
-  let result = true;
-  try {
-    tar.extract({ cwd: outputPath, 
-      file: tarGzFilePath, 
-      sync: true,
-      strict: true,
-      });
-  } catch (error) {
-    result = false;
-    cb?.(new Error(`An error occurred during TAR.GZ extraction: ${error}`));
+function extractArchive(archiveFilePath: string, outputPath: string): boolean {
+  let result = false;
+
+  console.log(`Extracting ${archiveFilePath}`);
+  const ext = path.extname(archiveFilePath).toLocaleLowerCase();
+  switch (ext) {
+    case ".zip":
+      result = extractZip(archiveFilePath, outputPath);
+      break;
+    case ".gz":
+      result = extractTarGz(archiveFilePath, outputPath);
+      break;
+    default:
+      throw new Error(`Unsupported archive extension: ${ext}`);
   }
 
   return result;
 }
 
+function extractZip(zipPath: string, outputPath: string): boolean {
+  try {
+    const zip = new AdmZip(zipPath);
+    zip.extractAllTo(outputPath, true, true);
+    return true;
+  } catch (error) {
+    console.error(`An error occurred during zip extraction: ${error}`);
+    return false;
+  }
+}
+
+function extractTarGz(tarGzFilePath: string, outputPath: string): boolean {
+  try {
+    tar.extract({
+      cwd: outputPath,
+      file: tarGzFilePath,
+      sync: true,
+      strict: true,
+    });
+    return true;
+  } catch (error) {
+    console.error(`An error occurred during tar.gz extraction: ${error}`);
+    return false;
+  }
+}
+
+function deleteArchive(archiveFilePath: string): void {
+  fs.unlinkSync(archiveFilePath);
+}
 
 function getVersion(filename: string): string {
-  const data = fs.readFileSync(filename, 'utf8');
+  const data = fs.readFileSync(filename, "utf8");
   const pkg = JSON.parse(data);
   return pkg.version;
 }
 
-function getAppFilename(version: string, os: string, cpu: string): string {
-  let compression = 'tar.gz';
-  switch (os) {
-    case 'windows':
-      platform = 'windows';
-      compression = 'zip';
+function getAppArchiveFilename(
+  version: string,
+  platform: string,
+  arch: string
+): string {
+  let compression = "zip";
+  switch (platform) {
+    case "windows":
+      platform = "windows";
       break;
-    case 'linux':
-      platform = 'linux';
+    case "linux":
+      platform = "linux";
+      compression = "tar.gz";
       break;
-    case 'darwin':
-      platform = 'macOS';
+    case "darwin":
+      platform = "macOS";
       break;
     default:
-      throw new Error(`Unsupported operating system: ${os}`);
+      throw new Error(`Unsupported operating system: ${platform}`);
   }
 
-  switch (cpu) {
-    case 'x64':
-      arch = 'amd64';
+  switch (arch) {
+    case "x64":
+      arch = "amd64";
       break;
-    case 'arm64':
-      arch = 'arm64';
+    case "arm64":
+      arch = "arm64";
       break;
     default:
       throw new Error(`Unsupported architecture: ${arch}`);
   }
 
-  if (platform === 'macOS') {
-    return `defang_${version}_${os}.${compression}`;
+  if (platform === "macOS") {
+    return `defang_${version}_${platform}.${compression}`;
   }
-  return `defang_${version}_${os}_${arch}.${compression}`;
+  return `defang_${version}_${platform}_${arch}.${compression}`;
 }
 
-function install() {
-  const version = getVersion('package.json');
-  const filename = getAppFilename(version, os.platform(), os.arch());
-  const destinationPath = path.join(__dirname, `../bin/${filename}`);
-  const archiveFile = downloadFile(version, filename, destinationPath);
-  if (archiveFile == "") {
-    console.error(`Failed to download ${filename}!`);
-    return;
-  }
-  
-  const result = extractArchive(archiveFile, '../bin/', console.error)
-  if (result === false) {
-    console.error(`Failed to install binaries!`);
-  } else {
-    console.log(`Successfully installed binaries!`);
+async function install() {
+  try {
+    console.log(`Starting install of defang cli`);
+
+    const version = getVersion("package.json");
+    const filename = getAppArchiveFilename(version, os.platform(), os.arch());
+    const archiveFile = await downloadAppArchive(version, filename, __dirname);
+    if (archiveFile.length === 0) {
+      throw new Error(`Failed to download ${filename}`);
+    }
+
+    const result = extractArchive(archiveFile, "./bin");
+    if (result === false) {
+      throw new Error(`Failed to install binaries!`);
+    }
+    console.log(`Successfully installed defang cli!`);
+    deleteArchive(archiveFile);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
   }
 }
 
