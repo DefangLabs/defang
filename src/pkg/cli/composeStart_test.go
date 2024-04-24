c6db35cbf287caeacfd06293b4cf421734ca8afe
Lionello Lunesu <lio+git@lunesu.com>
2024-04-24 13:09:13 -0700

    fix typo; add test

--- src/pkg/cli/composeStart_test.go
+++ src/pkg/cli/composeStart_test.go
@@ -5,9 +5,11 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
+	"slices"
 	"testing"
 
 	"github.com/defang-io/defang/src/pkg/cli/client"
+	defangv1 "github.com/defang-io/defang/src/protos/io/defang/v1"
 )
 
 func TestComposeStart(t *testing.T) {
@@ -27,5 +29,27 @@ func TestComposeStart(t *testing.T) {
 	_, err = ComposeStart(context.Background(), client.MockClient{UploadUrl: server.URL + "/"}, project, false)
 	if !errors.Is(err, ErrDryRun) {
 		t.Fatalf("ComposeStart() failed: %v", err)
+	}
+}
+
+func TestComposeFixupEnv(t *testing.T) {
+	project, err := LoadComposeWithProjectName("../../tests/fixupenv/compose.yaml", "tenant-id")
+	if err != nil {
+		t.Fatalf("LoadComposeWithProjectName() failed: %v", err)
+	}
+
+	services, err := convertServices(context.Background(), client.MockClient{}, project.Services, false)
+	if err != nil {
+		t.Fatalf("convertServices() failed: %v", err)
+	}
+	ui := slices.IndexFunc(services, func(s *defangv1.Service) bool { return s.Name == "ui" })
+	if ui < 0 {
+		t.Fatalf("convertServices() failed: expected service named 'ui'")
+	}
+
+	const expected = "http://mistral:8000"
+	got := services[ui].Environment["API_URL"]
+	if got != expected {
+		t.Errorf("convertServices() failed: expected API_URL=%s, got %s", expected, got)
 	}
 }
