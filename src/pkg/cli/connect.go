e8ef0339d6ab6dc9b7e54dcb45d1ec909ee2f933
Edward J <edw@defang.io>
2024-05-03 13:58:43 -0700

    Check tenant ID with server at connect

--- src/pkg/cli/connect.go
+++ src/pkg/cli/connect.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"context"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
@@ -60,7 +61,16 @@ func Connect(cluster string, loader client.ProjectLoader) (*client.GrpcClient, t
 	}
 	term.Debug(" - Using tenant", tenantId, "for cluster", host)
 
-	return client.NewGrpcClient(host, accessToken, tenantId, loader), tenantId
+	defangClient := client.NewGrpcClient(host, accessToken, tenantId, loader)
+	resp, err := defangClient.WhoAmI(context.Background()) // TODO: Should we pass in the command context?
+	if err != nil {
+		term.Debug(" - Unable to validate tenant ID with server:", err)
+	}
+	if resp != nil && tenantId != types.TenantID(resp.Tenant) {
+		term.Warnf(" - Overriding locally cached TenantID %v with server provided value %v", tenantId, resp.Tenant)
+		tenantId = types.TenantID(resp.Tenant)
+	}
+	return defangClient, tenantId
 }
 
 func NewClient(cluster string, provider client.Provider, loader client.ProjectLoader) client.Client {
