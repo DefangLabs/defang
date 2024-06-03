3795434b0c9c8ad47b7245589e0f9528b99ee452
Lionello Lunesu <lio+git@lunesu.com>
2024-06-02 17:47:52 -0700

    fix unit test in CI

--- src/pkg/cli/tail_test.go
+++ src/pkg/cli/tail_test.go
@@ -42,7 +42,7 @@ func TestParseTimeOrDuration(t *testing.T) {
 		{"2024-01-01T00:00:00Z", time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)},
 		{"2024-02-01T00:00:00.500Z", time.Date(2024, 2, 1, 0, 0, 0, 5e8, time.UTC)},
 		{"2024-03-01T00:00:00+07:00", time.Date(2024, 3, 1, 0, 0, 0, 0, time.FixedZone("", 7*60*60))},
-		{"01:02:03.040", time.Date(now.Year(), now.Month(), now.Day(), 1, 2, 3, 4e7, now.Location())},
+		{"00:01:02.040", time.Date(now.Year(), now.Month(), now.Day(), 0, 1, 2, 4e7, now.Location())}, // this test will fail if it's run at midnight UTC :(
 	}
 	for _, tt := range tdt {
 		t.Run(tt.td, func(t *testing.T) {
