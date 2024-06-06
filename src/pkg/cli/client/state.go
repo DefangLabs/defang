e7a7973486103e8b19e1cf520a39a2af84c07cc2
Edward J <edw@defang.io>
2024-06-06 14:53:18 -0700

    Move anonID gen/save/load logic to an overridable function

--- src/pkg/cli/client/state.go
+++ src/pkg/cli/client/state.go
@@ -1,11 +1,13 @@
 package client
 
 import (
+	"encoding/json"
 	"os"
 	"path/filepath"
 	"runtime"
 
 	"github.com/DefangLabs/defang/src/pkg"
+	"github.com/google/uuid"
 )
 
 func userStateDir() (string, error) {
@@ -21,6 +23,22 @@ var (
 	stateDir, _ = userStateDir()
 	// StateDir is the directory where the state file is stored
 	StateDir = filepath.Join(stateDir, "defang")
+
+	GetAnonID = func() string {
+		state := State{AnonID: uuid.NewString()}
+
+		// Restore anonID from config file
+		statePath := filepath.Join(StateDir, "state.json")
+		if bytes, err := os.ReadFile(statePath); err == nil {
+			json.Unmarshal(bytes, &state)
+		} else { // could be not found or path error
+			if bytes, err := json.MarshalIndent(state, "", "  "); err == nil {
+				os.MkdirAll(StateDir, 0700)
+				os.WriteFile(statePath, bytes, 0644)
+			}
+		}
+		return state.AnonID
+	}
 )
 
 type State struct {
