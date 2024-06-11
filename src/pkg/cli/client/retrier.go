a43211342d6720f613dfceeb1c3b315567ff65c6
Lionello Lunesu <lio+git@lunesu.com>
2024-06-11 15:30:10 -0700

    Auto-retry on Unavailable rpc

---
+++ src/pkg/cli/client/retrier.go
@@ -0,0 +1,30 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/DefangLabs/defang/src/pkg"
+	"github.com/bufbuild/connect-go"
+)
+
+type Retrier struct{}
+
+func (Retrier) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
+	return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
+		res, err := next(ctx, req)
+		if connect.CodeOf(err) == connect.CodeUnavailable {
+			// Retry once after a 1 second sleep
+			pkg.SleepWithContext(ctx, 1*time.Second)
+			res, err = next(ctx, req)
+		}
+		return res, err
+	}
+}
+func (Retrier) WrapStreamingClient(next connect.StreamingClientFunc) connect.StreamingClientFunc {
+	return next // TODO: wrap this to handle streaming rpcs like Tail
+}
+
+func (Retrier) WrapStreamingHandler(next connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
+	return next
+}
