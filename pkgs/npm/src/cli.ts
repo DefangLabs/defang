c792d78074cde09ed04836ffdfdbb2ca7d8e76a1
Eric Liu <eric.liu@defang.io>
2024-06-12 10:01:12 -0700

    initial skeleton

--- pkgs/npm/src/cli.ts
+++ pkgs/npm/src/cli.ts
@@ -2,6 +2,9 @@
 
 import * as path from "path";
 import * as child_process from "child_process";
+import { promisify } from "util";
+import { downloadAppArchive } from "./install";
+const exec = promisify(child_process.exec);
 
 const EXECUTABLE = "defang";
 
@@ -19,12 +22,45 @@ function getPathToExecutable(): string {
   }
 }
 
+function extractCLIVersions(output: string): {
+  defangCLI: string;
+  latestCLI: string;
+} {
+  const defangCLIRegex = /Defang CLI:\s+(\w+)/;
+  const latestCLIRegex = /Latest CLI:\s+(\S+)/;
+
+  const defangCLIMatch = output.match(defangCLIRegex);
+  const latestCLIMatch = output.match(latestCLIRegex);
+
+  if (defangCLIMatch && latestCLIMatch) {
+    return {
+      defangCLI: defangCLIMatch[1],
+      latestCLI: latestCLIMatch[1],
+    };
+  } else {
+    throw new Error("Could not extract CLI versions from the output.");
+  }
+}
+
+async function getVersionInfo(): Promise<{ current: string; latest: string }> {
+  const execPath = getPathToExecutable();
+
+  // Exec output contains both stderr and stdout outputs
+  const versionInfo = await exec(execPath + " --version");
+
+  const result = extractCLIVersions(versionInfo.stdout);
+
+  return { current: result.defangCLI, latest: result.latestCLI };
+}
+
 // js wrapper to use by npx or npm exec, this will call the defang binary with
 // the arguments passed to the npx line. NPM installer will create a symlink
 // in the user PATH to the cli.js to execute. The symlink will name the same as
 // the package name i.e. defang.
-function run(): void {
+async function run(): Promise<void> {
   try {
+    const { current, latest } = await getVersionInfo();
+
     const args = process.argv.slice(2);
     const processResult = child_process.spawnSync(getPathToExecutable(), args, {
       stdio: "inherit",
