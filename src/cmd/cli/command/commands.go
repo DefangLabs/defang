4dcf66c0f8f49dc76b262179cc1c8edd8cbf74e0
Edward J <edw@defang.io>
2024-06-20 12:18:56 -0700

    Allow InitFromSamples to load multiple samples

--- src/cmd/cli/command/commands.go
+++ src/cmd/cli/command/commands.go
@@ -434,7 +434,7 @@ var generateCmd = &cobra.Command{
 			if sample == "" {
 				return errors.New("cannot run in non-interactive mode")
 			}
-			return cli.InitFromSample(cmd.Context(), sample)
+			return cli.InitFromSamples(cmd.Context(), []string{sample})
 		}
 
 		if sample == "" {
@@ -550,7 +550,7 @@ Generate will write files in the current folder. You can edit them and then depl
 
 		if sample != "" {
 			term.Info("Fetching sample from the Defang repository...")
-			err := cli.InitFromSample(cmd.Context(), sample)
+			err := cli.InitFromSamples(cmd.Context(), []string{sample})
 			if err != nil {
 				return err
 			}
