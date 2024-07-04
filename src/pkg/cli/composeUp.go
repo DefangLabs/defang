49213fd1dcff76b458c3654a22247dedbf1b68b5
Lionello Lunesu <lio+git@lunesu.com>
2024-07-04 16:26:25 -0700

    wip

--- src/pkg/cli/composeUp.go
+++ src/pkg/cli/composeUp.go
@@ -46,20 +46,10 @@ func ComposeUp(ctx context.Context, c client.Client, force bool) (*defangv1.Depl
 		return nil, project, err
 	}
 
-	services := compose.ConvertServices(project.Services)
-	if len(services) == 0 {
-		return nil, project, &ComposeError{fmt.Errorf("no services found")}
-	}
-
 	if DoDryRun {
-		for _, service := range services {
-			PrintObject(service.Name, service)
-		}
+		yaml, _ := project.MarshalYAML()
+		fmt.Println(string(yaml))
 		return nil, project, ErrDryRun
-	}
-
-	for _, service := range services {
-		term.Info("Deploying service", service.Name)
 	}
 
 	bytes, err := project.MarshalYAML()
@@ -77,10 +67,13 @@ func ComposeUp(ctx context.Context, c client.Client, force bool) (*defangv1.Depl
 		return nil, project, err
 	}
 
+	for _, service := range project.Services {
+		term.Info("Deploying service", service.Name)
+	}
+
 	resp, err := c.Deploy(ctx, &defangv1.DeployRequest{
-		Project:  project.Name,
-		Services: services,
-		Compose:  str,
+		Project: project.Name,
+		Compose: str,
 	})
 	if err != nil {
 		return nil, project, err
