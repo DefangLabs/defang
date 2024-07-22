7246b633f6d0c87e1e16a675a7570048caac7092
edwardrf <edwardrf@gmail.com>
2024-07-22 10:04:11 -0700

    Update compose-go to remove hack to keep empty environment variables (#552)
    * Update compose-go to remove hack to keep empty environment variables
    
    * Fix nix hash
    
    * Remove unnecessary UDP warning logic
    
    ---------
    
    Co-authored-by: Edward J <edw@defang.io>

--- pkgs/defang/cli.nix
+++ pkgs/defang/cli.nix
@@ -6,7 +6,7 @@ buildGoModule {
   pname = "defang-cli";
   version = "git";
   src = ../../src;
-  vendorHash = "sha256-pQAMT6Gd4pPb5zCZDnOGdiYxAhuxUSYOqvFOSVysEOs=";
+  vendorHash = "sha256-DeaOcau1ZQk1lcEGrh5THQzNLbq1G33J8IyXo+F6i94=";
 
   subPackages = [ "cmd/cli" ];
 
