aa200cb3640b097c43f7627a98d8df861d21be75
Jordan Stephens <jordan@stephens.io>
2024-08-02 11:44:50 -0700

    Remove obsolete error type
    This error is no longer used

--- src/pkg/types/error.go
+++ src/pkg/types/error.go
@@ -3,4 +3,3 @@ package types
 import "errors"
 
 var ErrComposeFileNotFound = errors.New("no compose file found")
-var ErrMultipleComposeFilesFound = errors.New(`multiple Compose files found: ["./compose.yaml" "./docker-compose.yml"]; use -f to specify which one to use`)
