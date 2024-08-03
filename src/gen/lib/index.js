8ca0ddd3484c8a6386b6f69825b1945dc815f2c0
Lionello Lunesu <lio+git@lunesu.com>
2024-08-03 14:53:49 -0700

    remove js files

--- src/gen/lib/index.js
+++
@@ -1,5 +0,0 @@
-import * as pb from './io/defang/v1/fabric_pb';
-const update = pb.ProjectUpdate.fromJsonString('{"compose":{"services":{"web":{"image":"nginx"}}}}');
-const project = update.compose;
-console.log(project);
-//# sourceMappingURL=index.js.map
