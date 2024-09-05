316420160d23f9e38eb32cc5439c939965a36b1c
Lio李歐 <lionello@users.noreply.github.com>
2024-09-05 14:39:53 -0700

    Update src/cmd/cli/command/commands_test.go

--- src/cmd/cli/command/commands_test.go
+++ src/cmd/cli/command/commands_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestVersion(t *testing.T) {
-      if testing.Short() {
-            t.Skip("skipping test in short mode.")
-      }
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
 	err := testCommand([]string{"version"})
 	if err != nil {
 		t.Fatalf("Version() failed: %v", err)
