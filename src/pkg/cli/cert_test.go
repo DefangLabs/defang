f56ef1c6a53eb7d28c6f34df850ea03ffaf8d1cd
edwardrf <edwardrf@gmail.com>
2024-09-19 18:50:13 -0400

    Cache root resolver dns result for http client for 1m (#680)
    * Cache root resolver dns result for http client for 1m
    
    * Use longer delays and more tries when triggering cert gen
    
    To prevent let's encrypt DNS propagation delay caused verification
    failure
    
    * Use t.Cleanup instead of defer
    
    ---------
    
    Co-authored-by: Edward J <edw@defang.io>

--- src/pkg/cli/cert_test.go
+++ src/pkg/cli/cert_test.go
@@ -3,12 +3,16 @@ package cli
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"slices"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/DefangLabs/defang/src/pkg/dns"
 	"github.com/DefangLabs/defang/src/pkg/term"
@@ -290,10 +294,16 @@ func mockBody(s string) io.ReadCloser {
 }
 
 func TestGetWithRetries(t *testing.T) {
+	originalDelayBase := httpRetryDelayBase
+	httpRetryDelayBase = 100 * time.Millisecond
+	t.Cleanup(func() { httpRetryDelayBase = originalDelayBase })
+
 	t.Run("success on first try", func(t *testing.T) {
 		tc := &testClient{tries: []tryResult{
 			{result: &http.Response{StatusCode: 200, Body: mockBody("")}, err: nil},
 		}}
+		originalClient := httpClient
+		t.Cleanup(func() { httpClient = originalClient })
 		httpClient = tc
 		err := getWithRetries(context.Background(), "http://example.com", 3)
 		if err != nil {
@@ -309,6 +319,8 @@ func TestGetWithRetries(t *testing.T) {
 			{result: nil, err: errors.New("error")},
 			{result: &http.Response{StatusCode: 200, Body: mockBody("")}, err: nil},
 		}}
+		originalClient := httpClient
+		t.Cleanup(func() { httpClient = originalClient })
 		httpClient = tc
 		err := getWithRetries(context.Background(), "http://example.com", 3)
 		if err != nil {
@@ -324,6 +336,8 @@ func TestGetWithRetries(t *testing.T) {
 			{result: &http.Response{StatusCode: 503, Body: mockBody("Random Error")}, err: nil},
 			{result: nil, err: errors.New("error")},
 		}}
+		originalClient := httpClient
+		t.Cleanup(func() { httpClient = originalClient })
 		httpClient = tc
 		err := getWithRetries(context.Background(), "http://example.com", 3)
 		if err == nil {
@@ -341,6 +355,8 @@ func TestGetWithRetries(t *testing.T) {
 			{result: &http.Response{StatusCode: 502, Body: mockBody("Random Error")}, err: nil},
 			{result: &http.Response{StatusCode: 503, Body: mockBody("Random Error")}, err: nil},
 		}}
+		originalClient := httpClient
+		t.Cleanup(func() { httpClient = originalClient })
 		httpClient = tc
 		err := getWithRetries(context.Background(), "http://example.com", 3)
 		if err == nil {
@@ -358,6 +374,8 @@ func TestGetWithRetries(t *testing.T) {
 			{result: &http.Response{StatusCode: 502, Body: mockBody("Random Error")}, err: nil},
 			{result: &http.Response{StatusCode: 503, Body: mockBody("Random Error")}, err: nil},
 		}}
+		originalClient := httpClient
+		t.Cleanup(func() { httpClient = originalClient })
 		httpClient = tc
 		err := getWithRetries(context.Background(), "http://example.com", 1)
 		if err == nil {
@@ -367,4 +385,75 @@ func TestGetWithRetries(t *testing.T) {
 			t.Errorf("Expected 3 calls, got %v", tc.calls)
 		}
 	})
+}
+
+type MockResolver struct {
+	calls int
+}
+
+func (mr *MockResolver) LookupIPAddr(ctx context.Context, domain string) ([]net.IPAddr, error) {
+	mr.calls++
+	return []net.IPAddr{net.IPAddr{IP: net.ParseIP("127.0.0.1")}}, nil
+}
+func (mr *MockResolver) LookupCNAME(ctx context.Context, domain string) (string, error) {
+	mr.calls++
+	return "", nil
+}
+func (mr *MockResolver) LookupNS(ctx context.Context, domain string) ([]*net.NS, error) {
+	mr.calls++
+	return []*net.NS{&net.NS{Host: ""}}, nil
+}
+
+func TestHttpClient(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer ts.Close()
+	var mr MockResolver
+	resolver = &mr
+	dnsCacheDuration = 50 * time.Millisecond
+
+	tsu, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatalf("could not parse test server url '%v': %v", ts.URL, err)
+	}
+	_, port, err := net.SplitHostPort(tsu.Host)
+	if err != nil {
+		t.Fatalf("could not get test server port from '%v': %v", tsu.Host, err)
+	}
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://example.com:%v/", port), nil)
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		t.Fatalf("failed to make http call: %v", err)
+	}
+	resp.Body.Close()
+
+	if mr.calls != 1 {
+		t.Fatalf("expected 1 dns lookup, but got %v", mr.calls)
+	}
+
+	resp, err = httpClient.Do(req)
+	if err != nil {
+		t.Fatalf("failed to make http call: %v", err)
+	}
+	resp.Body.Close()
+	if mr.calls != 1 {
+		t.Fatalf("expected no increase in dns lookup, but got %v", mr.calls)
+	}
+
+	time.Sleep(80 * time.Millisecond)
+	resp, err = httpClient.Do(req)
+	if err != nil {
+		t.Fatalf("failed to make http call: %v", err)
+	}
+	resp.Body.Close()
+	if mr.calls != 2 {
+		t.Fatalf("expected 2nd dns lookup after cache expiry, but got %v", mr.calls)
+	}
+
 }
