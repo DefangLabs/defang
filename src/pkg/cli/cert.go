e0bd64b96289df2b5e5c5a305a3423ba5778bee1
edwardrf <edwardrf@gmail.com>
2024-10-10 10:52:42 -0700

    CheckTLSCert should check all IPs (#739)
    * CheckTLSCert should check all IPs
    
    * Make the verify dns target an array
    
    ---------
    
    Co-authored-by: Edward J <edw@defang.io>

--- src/pkg/cli/cert.go
+++ src/pkg/cli/cert.go
@@ -315,17 +315,46 @@ func CheckDomainDNSReady(ctx context.Context, domain string, validCNAMEs []strin
 }
 
 func CheckTLSCert(ctx context.Context, domain string) error {
-	url := fmt.Sprintf("https://%v", domain)
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	ips, err := resolver.LookupIPAddr(ctx, domain)
 	if err != nil {
 		return err
 	}
-	resp, err := httpClient.Do(req) // http non 200 errors are not considered as errors
-	if err != nil {
-		return err
+	for _, ip := range ips {
+		url := fmt.Sprintf("https://%v", domain)
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+		if err != nil {
+			return err
+		}
+		httpClient := &http.Client{
+			Transport: getFixedIPTransport(ip.String()),
+		}
+		resp, err := httpClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
 	}
-	defer resp.Body.Close()
 	return nil
+}
+
+func getFixedIPTransport(ip string) *http.Transport {
+	return &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			_, port, err := net.SplitHostPort(addr)
+			if err != nil {
+				return nil, err
+			}
+			dialer := &net.Dialer{}
+			rootAddr := net.JoinHostPort(ip, port)
+			return dialer.DialContext(ctx, network, rootAddr)
+		},
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
 }
 
 func getWithRetries(ctx context.Context, url string, tries int) error {
