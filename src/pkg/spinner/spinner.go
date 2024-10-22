be756baf6dc418f886181b29185eaad36c27196c
nullfunc <nullfunc@users.noreply.github.com>
2024-10-22 14:45:25 -0700

    show spinner via goroutine (#763)
    * update spinner to be it's own goroutine
    
    * update to use spinner.Start() in cert
    
    * review updates

--- src/pkg/spinner/spinner.go
+++ src/pkg/spinner/spinner.go
@@ -1,5 +1,11 @@
 package spinner
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
 var SpinnerChars = `-\|/`
 
 type Spinner struct {
@@ -14,4 +20,22 @@ func (s *Spinner) Next() string {
 	s.cnt++
 	runes := []rune(SpinnerChars)
 	return string([]rune{runes[s.cnt%len(runes)], '\b'})
+}
+
+func (s *Spinner) Start(ctx context.Context) context.CancelFunc {
+	cancelCtx, cancel := context.WithCancel(ctx)
+	go func(spinnerCtx context.Context) {
+		ticker := time.NewTicker(250 * time.Millisecond)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-spinnerCtx.Done():
+				return
+			case <-ticker.C:
+				fmt.Print(s.Next())
+			}
+		}
+	}(cancelCtx)
+
+	return cancel
 }
