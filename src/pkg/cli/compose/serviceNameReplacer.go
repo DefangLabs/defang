11aea4d3aee98f8a8136726c8dae48e1669aeb3b
nullfunc <nullfunc@users.noreply.github.com>
2024-10-24 13:57:34 -0700

    dns fixup in build args (#776)
    * extract service replacement function and apply to build args as well.
    
    * update var name to better reflect function
    
    * fix test
    
    * test case
    
    * code review updates
    
    * add unit tests
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * review updates: make serviceNameReplacer functions public. Update test to use FixUpTarget
    
    ---------
    
    Co-authored-by: Lio李歐 <lionello@users.noreply.github.com>
    Co-authored-by: Lionello Lunesu <lio+git@lunesu.com>

---
+++ src/pkg/cli/compose/serviceNameReplacer.go
@@ -0,0 +1,77 @@
+package compose
+
+import (
+	"regexp"
+	"slices"
+	"strings"
+
+	"github.com/DefangLabs/defang/src/pkg/cli/client"
+	"github.com/DefangLabs/defang/src/pkg/term"
+	defangv1 "github.com/DefangLabs/defang/src/protos/io/defang/v1"
+	compose "github.com/compose-spec/compose-go/v2/types"
+)
+
+type FixupTarget string
+
+const (
+	BuildArgs       FixupTarget = "build argument"
+	EnvironmentVars FixupTarget = "environment variable"
+)
+
+type ServiceNameReplacer struct {
+	client                     client.Client
+	nonReplaceServiceNameRegex *regexp.Regexp
+	serviceNameRegex           *regexp.Regexp
+}
+
+func NewServiceNameReplacer(client client.Client, services compose.Services) ServiceNameReplacer {
+	// Create a regexp to detect private service names in environment variable and build arg values
+	var serviceNames []string
+	var nonReplaceServiceNames []string
+	for _, svccfg := range services {
+		if network(&svccfg) == defangv1.Network_PRIVATE && slices.ContainsFunc(svccfg.Ports, func(p compose.ServicePortConfig) bool {
+			return p.Mode == "host" // only private services with host ports get DNS names
+		}) {
+			serviceNames = append(serviceNames, regexp.QuoteMeta(svccfg.Name))
+		} else {
+			nonReplaceServiceNames = append(nonReplaceServiceNames, regexp.QuoteMeta(svccfg.Name))
+		}
+	}
+
+	var serviceNameRegex *regexp.Regexp
+	if len(serviceNames) > 0 {
+		serviceNameRegex = regexp.MustCompile(`\b(?:` + strings.Join(serviceNames, "|") + `)\b`)
+	}
+	var nonReplaceServiceNameRegex *regexp.Regexp
+	if len(nonReplaceServiceNames) > 0 {
+		nonReplaceServiceNameRegex = regexp.MustCompile(`\b(?:` + strings.Join(nonReplaceServiceNames, "|") + `)\b`)
+	}
+
+	return ServiceNameReplacer{
+		client:                     client,
+		nonReplaceServiceNameRegex: nonReplaceServiceNameRegex,
+		serviceNameRegex:           serviceNameRegex,
+	}
+}
+
+func (s *ServiceNameReplacer) ReplaceServiceNameWithDNS(serviceName string, key, value string, fixupTarget FixupTarget) string {
+	val := value
+	if s.serviceNameRegex != nil && s.nonReplaceServiceNameRegex != nil {
+		// Replace service names with their actual DNS names; TODO: support public names too
+		val = s.serviceNameRegex.ReplaceAllStringFunc(value, func(serviceName string) string {
+			return s.client.ServiceDNS(NormalizeServiceName(serviceName))
+		})
+
+		if val != value {
+			term.Warnf("service %q: service name was adjusted: %s %q assigned value %q", serviceName, fixupTarget, key, val)
+		} else if s.nonReplaceServiceNameRegex != nil && s.nonReplaceServiceNameRegex.MatchString(value) {
+			term.Warnf("service %q: service name in the %s %q was not adjusted, only references to other services with port mode set to 'host' will be fixed-up", serviceName, fixupTarget, key)
+		}
+	}
+
+	return val
+}
+
+func (s *ServiceNameReplacer) HasServiceName(name string) bool {
+	return s.serviceNameRegex != nil && s.serviceNameRegex.MatchString(name)
+}
