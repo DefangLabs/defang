53c8433d4a48d67e4cbfa6e8582119b5841425d7
nullfunc <nullfunc@users.noreply.github.com>
2024-10-27 18:49:24 -0700

    byoc/playground config validation (#794)
    * working validation of config.
    
    * remove dupe compose types import
    
    * package name composeTypes
    
    ---------
    
    Co-authored-by: Lionello Lunesu <lio+git@lunesu.com>

--- src/pkg/cli/compose/validation_test.go
+++ src/pkg/cli/compose/validation_test.go
@@ -3,6 +3,7 @@ package compose
 import (
 	"bytes"
 	"context"
+	"errors"
 	"slices"
 	"strings"
 	"testing"
@@ -10,6 +11,8 @@ import (
 	"github.com/DefangLabs/defang/src/pkg/cli/client"
 	"github.com/DefangLabs/defang/src/pkg/term"
 	defangv1 "github.com/DefangLabs/defang/src/protos/io/defang/v1"
+	"github.com/aws/smithy-go/ptr"
+	composeTypes "github.com/compose-spec/compose-go/v2/types"
 )
 
 func TestValidationAndConvert(t *testing.T) {
@@ -26,19 +29,29 @@ func TestValidationAndConvert(t *testing.T) {
 
 		options := LoaderOptions{ConfigPaths: []string{path}}
 		loader := Loader{options: options}
-		proj, err := loader.LoadProject(context.Background())
+		project, err := loader.LoadProject(context.Background())
 		if err != nil {
 			t.Fatal(err)
 		}
-		if err := ValidateProject(proj); err != nil {
+
+		mockClient := validationMockClient{
+			configs: []string{"CONFIG1", "CONFIG2", "dummy", "ENV1", "SENSITIVE_DATA"},
+		}
+		listConfigNamesFunc := func(ctx context.Context) ([]string, error) {
+			configs, err := mockClient.ListConfig(ctx)
+			if err != nil {
+				return nil, err
+			}
+
+			return configs.Names, nil
+		}
+
+		if err := ValidateProject(project, listConfigNamesFunc); err != nil {
 			t.Logf("Project validation failed: %v", err)
 			logs.WriteString(err.Error() + "\n")
 		}
 
-		mockClient := MockClient{
-			configs: []string{"CONFIG1", "CONFIG2"},
-		}
-		if err := FixupServices(context.Background(), mockClient, proj.Services, UploadModeIgnore); err != nil {
+		if err := FixupServices(context.Background(), mockClient, project.Services, UploadModeIgnore); err != nil {
 			t.Logf("Service conversion failed: %v", err)
 			logs.WriteString(err.Error() + "\n")
 		}
@@ -55,18 +68,87 @@ func TestValidationAndConvert(t *testing.T) {
 	})
 }
 
-type MockClient struct {
+func TestValidateConfig(t *testing.T) {
+	const ENV_VAR = "ENV_VAR"
+
+	ctx := context.Background()
+	mockClient := validationMockClient{}
+
+	testProject := composeTypes.Project{
+		Services: composeTypes.Services{},
+	}
+
+	listConfigsNamesFunc := func(ctx context.Context) ([]string, error) {
+		configs, err := mockClient.ListConfig(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		return configs.Names, nil
+	}
+	t.Run("NOP", func(t *testing.T) {
+		env := map[string]*string{
+			ENV_VAR: ptr.String("blah"),
+		}
+
+		testProject.Services["service1"] = composeTypes.ServiceConfig{Environment: env}
+		if err := ValidateProjectConfig(ctx, &testProject, listConfigsNamesFunc); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("Missing Config", func(t *testing.T) {
+		var missing ErrMissingConfig
+		env := map[string]*string{
+			ENV_VAR: ptr.String("blah"),
+			"ASD":   nil,
+			"BSD":   nil,
+			"CSD":   nil,
+		}
+
+		ctx := context.Background()
+		testProject.Services["service1"] = composeTypes.ServiceConfig{Environment: env}
+		if err := ValidateProjectConfig(ctx, &testProject, listConfigsNamesFunc); !errors.As(err, &missing) {
+			t.Fatalf("uexpected ErrMissingConfig, got: %v", err)
+		} else {
+			if len(missing) != 3 {
+				t.Fatalf("unexpected error: number of missing, got: %d expected 3", len(missing))
+			}
+
+			for index, name := range []string{"ASD", "BSD", "CSD"} {
+				if missing[index] != name {
+					t.Fatalf("unexpected error: missing, got: %s expected ASD", missing[index])
+				}
+			}
+		}
+	})
+
+	t.Run("Valid Config", func(t *testing.T) {
+		const CONFIG_VAR = "CONFIG_VAR"
+		mockClient.configs = []string{CONFIG_VAR}
+		env := map[string]*string{
+			ENV_VAR:    ptr.String("blah"),
+			CONFIG_VAR: nil,
+		}
+		testProject.Services["service1"] = composeTypes.ServiceConfig{Environment: env}
+		if err := ValidateProjectConfig(ctx, &testProject, listConfigsNamesFunc); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+type validationMockClient struct {
 	client.Client
 	configs []string
 }
 
-func (m MockClient) ListConfig(ctx context.Context) (*defangv1.Secrets, error) {
+func (m validationMockClient) ListConfig(ctx context.Context) (*defangv1.Secrets, error) {
 	return &defangv1.Secrets{
 		Names:   m.configs,
 		Project: "mock-project",
 	}, nil
 }
 
-func (m MockClient) ServiceDNS(name string) string {
+func (m validationMockClient) ServiceDNS(name string) string {
 	return "mock-" + name
 }
