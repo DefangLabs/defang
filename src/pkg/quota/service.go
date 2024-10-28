ec3e8c741424edf7a99a4ca0c1dcd8fdb1393b03
Jordan Stephens <jordan@stephens.io>
2024-10-28 14:21:54 -0700

    partition quota and config validation (#797)

--- src/pkg/quota/service.go
+++ src/pkg/quota/service.go
@@ -17,7 +17,42 @@ type ServiceQuotas struct {
 	ShmSizeMiB float32
 }
 
-func (q ServiceQuotas) Validate(service *types.ServiceConfig) error {
+func (q ServiceQuotas) ValidateQuotas(service *types.ServiceConfig) error {
+	if service.Build != nil {
+		if shmSizeMiB := float32(service.Build.ShmSize) / compose.MiB; shmSizeMiB > q.ShmSizeMiB || service.Build.ShmSize < 0 {
+			return fmt.Errorf("build.shm_size %v MiB exceeds quota %v MiB", shmSizeMiB, q.ShmSizeMiB) // CodeInvalidArgument
+		}
+	}
+
+	if service.Deploy != nil {
+		if service.Deploy.Replicas != nil && *service.Deploy.Replicas > q.Replicas {
+			return fmt.Errorf("replicas exceeds quota (max %d)", q.Replicas) // CodeInvalidArgument
+		}
+		if service.Deploy.Resources.Reservations != nil {
+			if float32(service.Deploy.Resources.Reservations.NanoCPUs) > q.Cpus || service.Deploy.Resources.Reservations.NanoCPUs < 0 {
+				return fmt.Errorf("cpus exceeds quota (max %v vCPU)", q.Cpus) // CodeInvalidArgument
+			}
+			if memoryMiB := float32(service.Deploy.Resources.Reservations.MemoryBytes) / compose.MiB; memoryMiB > q.MemoryMiB || service.Deploy.Resources.Reservations.MemoryBytes < 0 {
+				return fmt.Errorf("memory %v MiB exceeds quota %v MiB", memoryMiB, q.MemoryMiB) // CodeInvalidArgument
+			}
+			for _, device := range service.Deploy.Resources.Reservations.Devices {
+				if len(device.Capabilities) != 1 || device.Capabilities[0] != "gpu" {
+					return errors.New("only GPU devices are supported") // CodeInvalidArgument
+				}
+				if device.Driver != "" && device.Driver != "nvidia" {
+					return errors.New("only nvidia GPU devices are supported") // CodeInvalidArgument
+				}
+				if q.Gpus == 0 || uint32(device.Count) > q.Gpus {
+					return fmt.Errorf("gpu count %v exceeds quota %d", device.Count, q.Gpus) // CodeInvalidArgument
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (q ServiceQuotas) ValidateService(service *types.ServiceConfig) error {
 	if service.Name == "" {
 		return errors.New("service name is required") // CodeInvalidArgument
 	}
@@ -25,9 +60,6 @@ func (q ServiceQuotas) Validate(service *types.ServiceConfig) error {
 	if service.Build != nil {
 		if service.Build.Context == "" {
 			return errors.New("build.context is required") // CodeInvalidArgument
-		}
-		if shmSizeMiB := float32(service.Build.ShmSize) / compose.MiB; shmSizeMiB > q.ShmSizeMiB || service.Build.ShmSize < 0 {
-			return fmt.Errorf("build.shm_size %v MiB exceeds quota %v MiB", shmSizeMiB, q.ShmSizeMiB) // CodeInvalidArgument
 		}
 	} else {
 		if service.Image == "" {
@@ -83,31 +115,6 @@ func (q ServiceQuotas) Validate(service *types.ServiceConfig) error {
 			}
 		default:
 			return fmt.Errorf("unsupported healthcheck: %v", service.HealthCheck.Test) // this will have been caught by compose-go
-		}
-	}
-
-	if service.Deploy != nil {
-		if service.Deploy.Replicas != nil && *service.Deploy.Replicas > q.Replicas {
-			return fmt.Errorf("replicas exceeds quota (max %d)", q.Replicas) // CodeInvalidArgument
-		}
-		if service.Deploy.Resources.Reservations != nil {
-			if float32(service.Deploy.Resources.Reservations.NanoCPUs) > q.Cpus || service.Deploy.Resources.Reservations.NanoCPUs < 0 {
-				return fmt.Errorf("cpus exceeds quota (max %v vCPU)", q.Cpus) // CodeInvalidArgument
-			}
-			if memoryMiB := float32(service.Deploy.Resources.Reservations.MemoryBytes) / compose.MiB; memoryMiB > q.MemoryMiB || service.Deploy.Resources.Reservations.MemoryBytes < 0 {
-				return fmt.Errorf("memory %v MiB exceeds quota %v MiB", memoryMiB, q.MemoryMiB) // CodeInvalidArgument
-			}
-			for _, device := range service.Deploy.Resources.Reservations.Devices {
-				if len(device.Capabilities) != 1 || device.Capabilities[0] != "gpu" {
-					return errors.New("only GPU devices are supported") // CodeInvalidArgument
-				}
-				if device.Driver != "" && device.Driver != "nvidia" {
-					return errors.New("only nvidia GPU devices are supported") // CodeInvalidArgument
-				}
-				if q.Gpus == 0 || uint32(device.Count) > q.Gpus {
-					return fmt.Errorf("gpu count %v exceeds quota %d", device.Count, q.Gpus) // CodeInvalidArgument
-				}
-			}
 		}
 	}
 
