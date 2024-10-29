719a8763cb86be47257dfe05d95c760a86e88517
Lio李歐 <lionello@users.noreply.github.com>
2024-10-28 18:03:40 -0700

    Smarter DNS replacement (#799)
    Co-authored-by: nullfunc <nullfunc@users.noreply.github.com>

--- src/pkg/cli/compose/serviceNameReplacer_test.go
+++ src/pkg/cli/compose/serviceNameReplacer_test.go
@@ -103,3 +103,34 @@ func TestServiceNameReplacerHasService(t *testing.T) {
 		t.Error("Expected to not have missing-service")
 	}
 }
+
+func TestMakeServiceNameRegex(t *testing.T) {
+	if makeServiceNameRegex(nil) != nil {
+		t.Error("makeServiceNameRegex(nil) != nil")
+	}
+
+	s := ServiceNameReplacer{
+		client:           serviceNameReplacerMockClient{},
+		hostServiceNames: makeServiceNameRegex([]string{"redis", "postgres"}),
+	}
+	tdt := []struct {
+		value    string
+		expected string
+	}{
+		{"nop", "nop"},
+		{"redis", "override-redis"},
+		{"redis:1234", "override-redis:1234"},
+		{"redis://redis", "redis://override-redis"},
+		{"redis://redis:6379", "redis://override-redis:6379"},
+		{"postgres", "override-postgres"},
+		{"postgres:1234", "override-postgres:1234"},
+		{"postgres://postgres", "postgres://override-postgres"},
+		{"pg://postgres:5432?u=postgres&p=password&d=nocodb", "pg://override-postgres:5432?u=postgres&p=password&d=nocodb"},
+		{"postgres://postgres:postgres@postgres:5432/postgres", "postgres://postgres:postgres@override-postgres:5432/postgres"},
+	}
+	for _, tt := range tdt {
+		if got := s.replaceServiceNameWithDNS(tt.value); got != tt.expected {
+			t.Errorf("makeServiceNameRegex(%q) expected %s, got %s", tt.value, tt.expected, got)
+		}
+	}
+}
