1b5240704c96e14caad91507df721aaaa910a253
Jordan Stephens <jordan@stephens.io>
2024-10-29 17:25:22 -0700

    Run go fmt with pre-commit hook (#801)
    * go fmt
    
    * setup a git hook to run go fmt pre-commit

--- src/pkg/clouds/aws/ecs/ecstaskstatechange/overrides_item.go
+++ src/pkg/clouds/aws/ecs/ecstaskstatechange/overrides_item.go
@@ -1,29 +1,29 @@
 package ecstaskstatechange
 
 type OverridesItem struct {
-    Environment []Environment `json:"environment,omitempty"`
-    Memory float64 `json:"memory,omitempty"`
-    Name string `json:"name"`
-    Cpu float64 `json:"cpu,omitempty"`
-    Command []string `json:"command,omitempty"`
+	Environment []Environment `json:"environment,omitempty"`
+	Memory      float64       `json:"memory,omitempty"`
+	Name        string        `json:"name"`
+	Cpu         float64       `json:"cpu,omitempty"`
+	Command     []string      `json:"command,omitempty"`
 }
 
 func (o *OverridesItem) SetEnvironment(environment []Environment) {
-    o.Environment = environment
+	o.Environment = environment
 }
 
 func (o *OverridesItem) SetMemory(memory float64) {
-    o.Memory = memory
+	o.Memory = memory
 }
 
 func (o *OverridesItem) SetName(name string) {
-    o.Name = name
+	o.Name = name
 }
 
 func (o *OverridesItem) SetCpu(cpu float64) {
-    o.Cpu = cpu
+	o.Cpu = cpu
 }
 
 func (o *OverridesItem) SetCommand(command []string) {
-    o.Command = command
+	o.Command = command
 }
