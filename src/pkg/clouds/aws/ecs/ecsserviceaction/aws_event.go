0a9de263a77a28fcf597b6aef1ce9e19092dc689
Jordan Stephens <jordan@stephens.io>
2024-10-29 17:25:22 -0700

    Run go fmt with pre-commit hook (#801)
    * go fmt
    
    * setup a git hook to run go fmt pre-commit

--- src/pkg/clouds/aws/ecs/ecsserviceaction/aws_event.go
+++ src/pkg/clouds/aws/ecs/ecsserviceaction/aws_event.go
@@ -1,54 +1,53 @@
 package ecsserviceaction
 
 import (
-    "time"
+	"time"
 )
 
-
 type AWSEvent struct {
-    Detail ECSServiceAction `json:"detail"`
-    Account string `json:"account"`
-    DetailType string `json:"detail-type"`
-    Id string `json:"id"`
-    Region string `json:"region"`
-    Resources []string `json:"resources"`
-    Source string `json:"source"`
-    Time time.Time `json:"time"`
-    Version string `json:"version"`
+	Detail     ECSServiceAction `json:"detail"`
+	Account    string           `json:"account"`
+	DetailType string           `json:"detail-type"`
+	Id         string           `json:"id"`
+	Region     string           `json:"region"`
+	Resources  []string         `json:"resources"`
+	Source     string           `json:"source"`
+	Time       time.Time        `json:"time"`
+	Version    string           `json:"version"`
 }
 
 func (a *AWSEvent) SetDetail(detail ECSServiceAction) {
-    a.Detail = detail
+	a.Detail = detail
 }
 
 func (a *AWSEvent) SetAccount(account string) {
-    a.Account = account
+	a.Account = account
 }
 
 func (a *AWSEvent) SetDetailType(detailType string) {
-    a.DetailType = detailType
+	a.DetailType = detailType
 }
 
 func (a *AWSEvent) SetId(id string) {
-    a.Id = id
+	a.Id = id
 }
 
 func (a *AWSEvent) SetRegion(region string) {
-    a.Region = region
+	a.Region = region
 }
 
 func (a *AWSEvent) SetResources(resources []string) {
-    a.Resources = resources
+	a.Resources = resources
 }
 
 func (a *AWSEvent) SetSource(source string) {
-    a.Source = source
+	a.Source = source
 }
 
 func (a *AWSEvent) SetTime(time time.Time) {
-    a.Time = time
+	a.Time = time
 }
 
 func (a *AWSEvent) SetVersion(version string) {
-    a.Version = version
+	a.Version = version
 }
--- src/pkg/clouds/aws/ecs/ecstaskstatechange/container_details.go
+++ src/pkg/clouds/aws/ecs/ecstaskstatechange/container_details.go
@@ -1,79 +1,79 @@
 package ecstaskstatechange
 
 type ContainerDetails struct {
-    Image string `json:"image,omitempty"`
-    ImageDigest string `json:"imageDigest,omitempty"`
-    NetworkInterfaces []NetworkInterfaceDetails `json:"networkInterfaces,omitempty"`
-    NetworkBindings []NetworkBindingDetails `json:"networkBindings,omitempty"`
-    Memory string `json:"memory,omitempty"`
-    MemoryReservation string `json:"memoryReservation,omitempty"`
-    TaskArn string `json:"taskArn"`
-    Name string `json:"name"`
-    ExitCode float64 `json:"exitCode,omitempty"`
-    Cpu string `json:"cpu,omitempty"`
-    ContainerArn string `json:"containerArn"`
-    LastStatus string `json:"lastStatus"`
-    RuntimeId string `json:"runtimeId,omitempty"`
-    Reason string `json:"reason,omitempty"`
-    GpuIds []string `json:"gpuIds,omitempty"`
+	Image             string                    `json:"image,omitempty"`
+	ImageDigest       string                    `json:"imageDigest,omitempty"`
+	NetworkInterfaces []NetworkInterfaceDetails `json:"networkInterfaces,omitempty"`
+	NetworkBindings   []NetworkBindingDetails   `json:"networkBindings,omitempty"`
+	Memory            string                    `json:"memory,omitempty"`
+	MemoryReservation string                    `json:"memoryReservation,omitempty"`
+	TaskArn           string                    `json:"taskArn"`
+	Name              string                    `json:"name"`
+	ExitCode          float64                   `json:"exitCode,omitempty"`
+	Cpu               string                    `json:"cpu,omitempty"`
+	ContainerArn      string                    `json:"containerArn"`
+	LastStatus        string                    `json:"lastStatus"`
+	RuntimeId         string                    `json:"runtimeId,omitempty"`
+	Reason            string                    `json:"reason,omitempty"`
+	GpuIds            []string                  `json:"gpuIds,omitempty"`
 }
 
 func (c *ContainerDetails) SetImage(image string) {
-    c.Image = image
+	c.Image = image
 }
 
 func (c *ContainerDetails) SetImageDigest(imageDigest string) {
-    c.ImageDigest = imageDigest
+	c.ImageDigest = imageDigest
 }
 
 func (c *ContainerDetails) SetNetworkInterfaces(networkInterfaces []NetworkInterfaceDetails) {
-    c.NetworkInterfaces = networkInterfaces
+	c.NetworkInterfaces = networkInterfaces
 }
 
 func (c *ContainerDetails) SetNetworkBindings(networkBindings []NetworkBindingDetails) {
-    c.NetworkBindings = networkBindings
+	c.NetworkBindings = networkBindings
 }
 
 func (c *ContainerDetails) SetMemory(memory string) {
-    c.Memory = memory
+	c.Memory = memory
 }
 
 func (c *ContainerDetails) SetMemoryReservation(memoryReservation string) {
-    c.MemoryReservation = memoryReservation
+	c.MemoryReservation = memoryReservation
 }
 
 func (c *ContainerDetails) SetTaskArn(taskArn string) {
-    c.TaskArn = taskArn
+	c.TaskArn = taskArn
 }
 
 func (c *ContainerDetails) SetName(name string) {
-    c.Name = name
+	c.Name = name
 }
 
 func (c *ContainerDetails) SetExitCode(exitCode float64) {
-    c.ExitCode = exitCode
+	c.ExitCode = exitCode
 }
 
 func (c *ContainerDetails) SetCpu(cpu string) {
-    c.Cpu = cpu
+	c.Cpu = cpu
 }
 
 func (c *ContainerDetails) SetContainerArn(containerArn string) {
-    c.ContainerArn = containerArn
+	c.ContainerArn = containerArn
 }
 
 func (c *ContainerDetails) SetLastStatus(lastStatus string) {
-    c.LastStatus = lastStatus
+	c.LastStatus = lastStatus
 }
 
 func (c *ContainerDetails) SetRuntimeId(runtimeId string) {
-    c.RuntimeId = runtimeId
+	c.RuntimeId = runtimeId
 }
 
 func (c *ContainerDetails) SetReason(reason string) {
-    c.Reason = reason
+	c.Reason = reason
 }
 
 func (c *ContainerDetails) SetGpuIds(gpuIds []string) {
-    c.GpuIds = gpuIds
+	c.GpuIds = gpuIds
 }
