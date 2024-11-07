876a22ea769390b191341cee27ac53019d06e0b8
Lio李歐 <lionello@users.noreply.github.com>
2024-11-07 14:39:48 -0800

    Copy LoadFromContent from MVP (#838)

--- src/pkg/cli/compose/load_content.go
+++ src/pkg/cli/compose/load_content.go
@@ -7,8 +7,9 @@ import (
 	composeTypes "github.com/compose-spec/compose-go/v2/types"
 )
 
-func LoadFromContent(ctx context.Context, content []byte) (*Project, error) {
+func LoadFromContent(ctx context.Context, content []byte, nameFallback string) (*Project, error) {
 	return loader.LoadWithContext(ctx, composeTypes.ConfigDetails{ConfigFiles: []composeTypes.ConfigFile{{Content: content}}}, func(o *loader.Options) {
+		o.SetProjectName(nameFallback, false)
 		o.SkipConsistencyCheck = true // this matches the WithConsistency(false) option from the loader
 		o.SkipInterpolation = true
 		o.SkipResolveEnvironment = true
