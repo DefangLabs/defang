1f4516c0c6539b1d1f7890584177e166c109036c
Linda Lee <llee008844@gmail.com>
2024-11-20 13:15:25 -0800

    Removed http from url in curl command (#878)
    * remove http from url in curl
    
    * refine install

--- src/pkg/cli/upgrade.go
+++ src/pkg/cli/upgrade.go
@@ -49,7 +49,7 @@ func Upgrade(ctx context.Context) error {
 	}
 
 	// Default to the shell script
-	printInstructions(`eval "$(curl -fsSL https://s.defang.io/install)"`)
+	printInstructions(`eval "$(curl -fsSL s.defang.io/install)"`)
 
 	return nil
 }
