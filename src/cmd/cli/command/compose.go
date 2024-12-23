c59ee226c3f5dcf001466878192ef544d718a8d4
Lionello Lunesu <lio+git@lunesu.com>
2024-12-23 10:40:12 -0800

    Fix #1300: more clear error message

--- src/cmd/cli/command/compose.go
+++ src/cmd/cli/command/compose.go
@@ -145,7 +145,7 @@ func makeComposeUpCmd() *cobra.Command {
 					if errors.As(err, &errDeploymentFailed) {
 						cancelTail(err)
 					} else if !(errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded)) {
-						term.Warnf("failed to wait for service status: %v", err) // TODO: don't print in Go-routine
+						term.Warnf("error waiting for deployment completion: %v", err) // TODO: don't print in Go-routine
 					}
 				} else {
 					cancelTail(errCompleted)
