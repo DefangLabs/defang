361872b52ef9a4de264312b19744c353ec571dee
Eric Liu <eric.liu@defang.io>
2025-01-24 15:41:30 -0800

    add some commonly used shell commands

--- flake.nix
+++ flake.nix
@@ -1,33 +1,30 @@
 {
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in
-        {
-          devShell = with pkgs; mkShell {
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = with pkgs;
+          mkShell {
             buildInputs = [
               buf
               crane
               git
               gnumake
+              gnupg
               gnused # force Linux `sed` everywhere
               go_1_22
               golangci-lint
               goreleaser
+              less
               nixfmt
               nodejs_20 # for Pulumi, must match values in package.json
               pulumi-bin
               google-cloud-sdk
-            ] ++ lib.optionals stdenv.isDarwin [
-              darwin.apple_sdk.frameworks.CoreServices
-            ];
+              vim
+            ] ++ lib.optionals stdenv.isDarwin
+              [ darwin.apple_sdk.frameworks.CoreServices ];
           };
-          packages.defang-cli = pkgs.callPackage ./pkgs/defang/cli.nix { };
-          packages.defang-bin = pkgs.callPackage ./pkgs/defang { };
-        }
-      );
+        packages.defang-cli = pkgs.callPackage ./pkgs/defang/cli.nix { };
+        packages.defang-bin = pkgs.callPackage ./pkgs/defang { };
+      });
 }
