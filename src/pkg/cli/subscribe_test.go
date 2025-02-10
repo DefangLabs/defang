bfa467ee4e7a02d80132564b6c93f72e6c480844
yuta519 <ytkwmr18@icloud.com>
2025-02-10 10:17:42 -0800

    Add tests to retry to connect

--- src/pkg/cli/subscribe_test.go
+++ src/pkg/cli/subscribe_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/DefangLabs/defang/src/pkg"
 	"github.com/DefangLabs/defang/src/pkg/cli/client"
 	defangv1 "github.com/DefangLabs/defang/src/protos/io/defang/v1"
+	"github.com/bufbuild/connect-go"
 )
 
 // MockSubscribeServerStream mocks the stream response for Subscribe.
@@ -235,5 +236,99 @@ func TestWaitServiceState(t *testing.T) {
 
 	if len(provider.Reqs) == 0 {
 		t.Errorf("Expected Subscribe to be called but got 0 requests")
+	}
+}
+
+type MockSubscribeServerStreamForReconnectTest struct {
+	Error error
+	retry int
+}
+
+func (*MockSubscribeServerStreamForReconnectTest) Close() error {
+	return nil
+}
+
+func (m *MockSubscribeServerStreamForReconnectTest) Receive() bool {
+	return false
+}
+
+func (m *MockSubscribeServerStreamForReconnectTest) Msg() *defangv1.SubscribeResponse {
+	return nil
+}
+
+func (m *MockSubscribeServerStreamForReconnectTest) Err() error {
+	if m.retry < 5 {
+		m.retry++
+		return m.Error
+	}
+	return connect.NewError(connect.CodeCanceled, errors.New("cancel connect error")) // cancel the connection after 5 retries to avoid infinite loop
+}
+
+type mockSubscribeProviderForReconnectTest struct {
+	client.MockProvider
+	stream *MockSubscribeServerStreamForReconnectTest
+}
+
+func (m *mockSubscribeProviderForReconnectTest) Subscribe(
+	_ context.Context,
+	_ *defangv1.SubscribeRequest,
+) (client.ServerStream[defangv1.SubscribeResponse], error) {
+	return m.stream, nil
+}
+
+func (m *mockSubscribeProviderForReconnectTest) Receive() bool {
+	return false
+}
+
+func (m *mockSubscribeProviderForReconnectTest) Err() bool {
+	return false
+}
+
+func TestWaitServiceStateStreamReceive(t *testing.T) {
+	tests := []struct {
+		name        string
+		stream      *MockSubscribeServerStreamForReconnectTest
+		expectRetry bool
+	}{
+		{
+			name: "stream receive returns permission denied error and not retry to connect",
+			stream: &MockSubscribeServerStreamForReconnectTest{
+				Error: connect.NewError(connect.CodePermissionDenied, errors.New("Not Transient Error")),
+			},
+			expectRetry: false,
+		},
+		{
+			name: "stream receive returns unavailable error and retry to connect",
+			stream: &MockSubscribeServerStreamForReconnectTest{
+				Error: connect.NewError(connect.CodeUnavailable, errors.New("stream error")),
+			},
+			expectRetry: true,
+		},
+		{
+			name: "stream receive returns internal error and retry to connect",
+			stream: &MockSubscribeServerStreamForReconnectTest{
+				Error: connect.NewError(connect.CodeInternal, errors.New("internal error")),
+			},
+			expectRetry: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			provider := &mockSubscribeProviderForReconnectTest{stream: tt.stream}
+			err := WaitServiceState(
+				ctx, provider,
+				defangv1.ServiceState_DEPLOYMENT_COMPLETED,
+				"EtagSomething",
+				[]string{"service1"},
+			)
+			if !tt.expectRetry && isTransientError(err) && provider.stream.retry > 5 {
+				t.Errorf("unexpected error: %v", err)
+			}
+			if tt.expectRetry && err == nil && provider.stream.retry < 5 {
+				t.Error("expected error but got nil")
+			}
+		})
 	}
 }
