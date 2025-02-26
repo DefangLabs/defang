e0f4f715d6f8d012785056ab12fe5d29a1fa726d
Jordan Stephens <jordan@stephens.io>
2025-02-26 15:32:39 -0800

    add GetProjectUpdate to Provider interface (#1035)
    * add GetProjectUpdate to Provider interface
    
    * pass FabricClient to WaitAndTail

--- src/pkg/cli/client/provider.go
+++ src/pkg/cli/client/provider.go
@@ -131,6 +131,7 @@ type Provider interface {
 	DelayBeforeRetry(context.Context) error
 	Destroy(context.Context, *defangv1.DestroyRequest) (types.ETag, error)
 	Follow(context.Context, *defangv1.TailRequest) (ServerStream[defangv1.TailResponse], error)
+	GetProjectUpdate(context.Context, string) (*defangv1.ProjectUpdate, error)
 	GetService(context.Context, *defangv1.GetRequest) (*defangv1.ServiceInfo, error)
 	GetServices(context.Context, *defangv1.GetServicesRequest) (*defangv1.GetServicesResponse, error)
 	ListConfig(context.Context, *defangv1.ListConfigsRequest) (*defangv1.Secrets, error)
