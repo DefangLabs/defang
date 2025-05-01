dd0373034621ec961e29b4f1894854775b40b50a
Lio李歐 <lionello@users.noreply.github.com>
2025-05-01 09:14:08 -0700

    Update cli.nix vendorHash

--- pkgs/defang/cli.nix
+++ pkgs/defang/cli.nix
@@ -7,7 +7,7 @@ buildGoModule {
   pname = "defang-cli";
   version = "git";
   src = ../../src;
-  vendorHash = "sha256-bDQcUL/ecaTHyvsRtSty+AV49g2onX1mmudqvQ+K2Z4="; # TODO: use fetchFromGitHub
+  vendorHash = "sha256-Q9xskoubg/RnrYD2PISWpZ8VagSJtcmG4WOUJHb05p4="; # TODO: use fetchFromGitHub
 
   subPackages = [ "cmd/cli" ];
 
