0fc116709b03a2007aa2b6083cb4d6cdafda864f
Edward J <edw@defang.io>
2025-05-30 13:16:34 -0700

    Add SaveLabelValue function for gcp

---
+++ src/pkg/clouds/gcp/label.go
@@ -0,0 +1,24 @@
+package gcp
+
+import (
+	"regexp"
+	"strings"
+)
+
+// SafeValue converts a string to a safe value for use in labels.
+//
+// GCP Label value requirement:
+//   - The value can only contain lowercase letters, numeric characters, underscores and dashes.
+//   - The value can be at most 63 characters long.
+//   - International characters are allowed.
+//
+// Even though the GCP allow "international characters" in labels, we use a subset for simplicity
+func SafeLabelValue(input string) string {
+	input = strings.ToLower(input)
+	re := regexp.MustCompile(`[^a-z0-9_-]+`)
+	safe := re.ReplaceAllString(input, "-")
+	if len(safe) > 63 {
+		safe = safe[:63]
+	}
+	return safe
+}
