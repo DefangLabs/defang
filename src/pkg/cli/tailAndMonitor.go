1c71328149a31b4c50ea4b9322b5d1f7f07ede8e
Edward J <edw@defang.io>
2025-06-04 10:11:51 -0700

    Handle GCP transient errors

--- src/pkg/cli/tailAndMonitor.go
+++ src/pkg/cli/tailAndMonitor.go
@@ -43,17 +43,32 @@ func TailAndMonitor(ctx context.Context, project *compose.Project, provider clie
 	go func() {
 		defer wg.Done()
 		// block on waiting for services to reach target state
-		serviceStates, svcErr = WaitServiceState(svcStatusCtx, provider, targetServiceState, project.Name, tailOptions.Deployment, computeServices)
+		for {
+			serviceStates, svcErr = WaitServiceState(svcStatusCtx, provider, targetServiceState, project.Name, tailOptions.Deployment, computeServices)
+			if svcErr != nil && isTransientError(svcErr) {
+				term.Debug("WaitServiceState failed with transient error, retrying:", svcErr)
+				continue
+			}
+			break
+		}
+		term.Debug("WaitServiceState stopped with", svcErr)
 	}()
 
 	go func() {
 		defer wg.Done()
 		// block on waiting for cdTask to complete
-		if err := client.WaitForCdTaskExit(ctx, provider); err != nil {
+		for {
+			err := client.WaitForCdTaskExit(ctx, provider)
+			if err != nil && isTransientError(err) {
+				term.Debug("WaitForCdTaskExit failed with transient error, retrying:", err)
+				continue
+			}
 			cdErr = err
 			// When CD fails, stop WaitServiceState
 			cancelSvcStatus(cdErr)
+			break
 		}
+		term.Debug("WaitForCdTaskExit stopped with", cdErr)
 	}()
 
 	errMonitoringDone := errors.New("monitoring done") // pseudo error to signal that monitoring is done
