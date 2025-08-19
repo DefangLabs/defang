45e9dec76261a0e2887b270bf45907694025f146
Jordan Stephens <jordan@stephens.io>
2025-08-19 16:16:59 -0700

    factor out RequireLoginAndToS

--- src/cmd/cli/command/commands.go
+++ src/cmd/cli/command/commands.go
@@ -380,14 +380,51 @@ var RootCmd = &cobra.Command{
 			return nil
 		}
 
-		if nonInteractive {
-			err = client.CheckLoginAndToS(ctx)
-		} else {
-			err = login.InteractiveRequireLoginAndToS(ctx, client, getCluster())
-		}
+		err = RequireLoginAndToS(cmd.Context())
 
 		return err
 	},
+}
+
+func RequireLoginAndToS(ctx context.Context) error {
+	var err error
+	if err = client.CheckLoginAndToS(ctx); err != nil {
+		if nonInteractive {
+			return err
+		}
+		// Login interactively now; only do this for authorization-related errors
+		if connect.CodeOf(err) == connect.CodeUnauthenticated {
+			term.Debug("Server error:", err)
+			term.Warn("Please log in to continue.")
+			term.ResetWarnings() // clear any previous warnings so we don't show them again
+
+			defer func() { track.Cmd(nil, "Login", P("reason", err)) }()
+			if err = login.InteractiveLogin(ctx, client, getCluster()); err != nil {
+				return err
+			}
+
+			// Reconnect with the new token
+			if client, err = cli.Connect(ctx, getCluster()); err != nil {
+				return err
+			}
+
+			if err = client.CheckLoginAndToS(ctx); err == nil { // recheck (new token = new user)
+				return nil // success
+			}
+		}
+
+		// Check if the user has agreed to the terms of service and show a prompt if needed
+		if connect.CodeOf(err) == connect.CodeFailedPrecondition {
+			term.Warn(prettyError(err))
+
+			defer func() { track.Cmd(nil, "Terms", P("reason", err)) }()
+			if err = login.InteractiveAgreeToS(ctx, client); err != nil {
+				return err // fatal
+			}
+		}
+	}
+
+	return err
 }
 
 var loginCmd = &cobra.Command{
