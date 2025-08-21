d7d75d7042522571d6caeeea3d80b8509b6d45a2
Jordan Stephens <jordan@stephens.io>
2025-08-21 10:02:02 -0700

    preserve variable interpolation when validating generated compose files

--- src/pkg/cli/compose/load_content.go
+++ src/pkg/cli/compose/load_content.go
@@ -7,12 +7,20 @@ import (
 	composeTypes "github.com/compose-spec/compose-go/v2/types"
 )
 
-func LoadFromContent(ctx context.Context, content []byte, nameFallback string) (*Project, error) {
+func loadFromContent(ctx context.Context, content []byte, nameFallback string, skipInterpolation bool) (*Project, error) {
 	return loader.LoadWithContext(ctx, composeTypes.ConfigDetails{ConfigFiles: []composeTypes.ConfigFile{{Content: content}}}, func(o *loader.Options) {
 		o.SetProjectName(nameFallback, false)
-		o.SkipConsistencyCheck = true // this matches the WithConsistency(false) option from the loader
-		o.SkipInterpolation = true
+		o.SkipConsistencyCheck = true
+		o.SkipInterpolation = skipInterpolation
 		o.SkipResolveEnvironment = true
 		o.SkipInclude = true
 	})
+}
+
+func LoadFromContent(ctx context.Context, content []byte, nameFallback string) (*Project, error) {
+	return loadFromContent(ctx, content, nameFallback, true)
+}
+
+func LoadFromContentWithInterpolation(ctx context.Context, content []byte, nameFallback string) (*Project, error) {
+	return loadFromContent(ctx, content, nameFallback, false)
 }
