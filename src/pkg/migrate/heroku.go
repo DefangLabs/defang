3cd76874ce5a1d9fd6910f60c370a43effe2d9d9
Jordan Stephens <jordan@stephens.io>
2025-08-22 10:23:11 -0700

    typo fix
    Co-authored-by: Lio李歐 <lionello@users.noreply.github.com>

--- src/pkg/migrate/heroku.go
+++ src/pkg/migrate/heroku.go
@@ -388,7 +388,7 @@ func getHerokuAuthToken() (string, error) {
 	for {
 		err := survey.AskOne(&survey.Password{
 			Message: "Please paste a Heroku auth token, so Defang can collect information about your applications",
-			Help:    "Run `Visit https://dashboard.heroku.com/account/applications/authorizations/new",
+			Help:    "Visit https://dashboard.heroku.com/account/applications/authorizations/new",
 		}, &token)
 		if err != nil {
 			return "", fmt.Errorf("failed to prompt for Heroku token: %w", err)
