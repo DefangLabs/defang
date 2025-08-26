bbe754c64503ff5981e59223423ab7963f1f6666
Lionello Lunesu <lio+git@lunesu.com>
2025-08-26 16:35:39 -0700

    chore: use canonical header names

--- src/pkg/auth/interceptor.go
+++ src/pkg/auth/interceptor.go
@@ -7,7 +7,7 @@ import (
 	"github.com/bufbuild/connect-go"
 )
 
-const XDefangOrgID = "x-defang-orgid"
+const XDefangOrgID = "X-Defang-Orgid"
 
 type authInterceptor struct {
 	authorization string
@@ -20,8 +20,8 @@ func NewAuthInterceptor(token, orgID string) connect.Interceptor {
 
 func (a *authInterceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
 	return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
-		req.Header().Set("authorization", a.authorization)
-		req.Header().Set("content-type", "application/grpc") // same as the gRPC client
+		req.Header().Set("Authorization", a.authorization)
+		req.Header().Set("Content-Type", "application/grpc") // same as the gRPC client
 		req.Header().Set(XDefangOrgID, a.orgID)
 		return next(ctx, req)
 	}
@@ -30,8 +30,8 @@ func (a *authInterceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
 func (a *authInterceptor) WrapStreamingClient(next connect.StreamingClientFunc) connect.StreamingClientFunc {
 	return func(ctx context.Context, spec connect.Spec) connect.StreamingClientConn {
 		conn := next(ctx, spec)
-		conn.RequestHeader().Set("authorization", a.authorization)
-		conn.RequestHeader().Set("content-type", "application/grpc") // same as the gRPC client
+		conn.RequestHeader().Set("Authorization", a.authorization)
+		conn.RequestHeader().Set("Content-Type", "application/grpc") // same as the gRPC client
 		conn.RequestHeader().Set(XDefangOrgID, a.orgID)
 		return conn
 	}
