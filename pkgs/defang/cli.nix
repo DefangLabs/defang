6c87f596ade5eb056309fb86ea8bad7d8f66dffc
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2025-09-20 13:45:02 +0200

    update nix/go

--- pkgs/defang/cli.nix
+++ pkgs/defang/cli.nix
@@ -7,7 +7,7 @@ buildGoModule {
   pname = "defang-cli";
   version = "git";
   src = ../../src;
-  vendorHash = "sha256-m8xBhpKS6Tvoik2WZbkWgM52/FkCEhbdFNRwByi30zw="; # TODO: use fetchFromGitHub
+  vendorHash = "sha256-VYJjG99g66S8V6GzqdZngnHESaVkQ4C6Tl2VsFMVCLA="; # TODO: use fetchFromGitHub
 
   subPackages = [ "cmd/cli" ];
 
