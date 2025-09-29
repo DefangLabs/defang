ef5e9f2c7c143ca981280c2ab91aa94f1593b0b9
Eric Liu <eric.liu@defang.io>
2025-09-28 23:59:37 -0700

    make separate gcp and playground providers logic

---
+++ src/pkg/mcp/actions/setGCPBYOCprovider.go
@@ -0,0 +1,36 @@
+package actions
+
+import (
+	"context"
+	"os"
+
+	"github.com/DefangLabs/defang/src/pkg/cli/client"
+	"github.com/DefangLabs/defang/src/pkg/mcp/common"
+)
+
+func SetGCPByocProvider(ctx context.Context, cluster string, projectID string, providerId *client.ProviderID) error {
+	err := os.Setenv("GCP_PROJECT_ID", projectID)
+	if err != nil {
+		return err
+	}
+
+	fabric, err := common.Connect(ctx, cluster)
+	if err != nil {
+		return err
+	}
+
+	_, err = common.CheckProviderConfigured(ctx, fabric, client.ProviderGCP, "", 0)
+	if err != nil {
+		return err
+	}
+
+	*providerId = client.ProviderGCP
+
+	//FIXME: Should not be setting both the global var and env var
+	err = os.Setenv("DEFANG_PROVIDER", "gcp")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
