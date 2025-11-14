8de2af32130a266e13be8b2c30dceb3b5ae75de0
Jordan Stephens <jordan@stephens.io>
2025-11-14 15:13:35 -0800

    split tool output to buffer and stdout

--- src/pkg/agent/tools/capture.go
+++ src/pkg/agent/tools/capture.go
@@ -2,6 +2,7 @@ package tools
 
 import (
 	"bytes"
+	"io"
 	"os"
 
 	"github.com/DefangLabs/defang/src/pkg/term"
@@ -10,18 +11,19 @@ import (
 func CaptureTerm(f func() (string, error)) (string, error) {
 	// replace the default term with a new term that writes to a buffer
 	originalTerm := term.DefaultTerm
-	outStream := bytes.NewBuffer(nil)
-	errStream := bytes.NewBuffer(nil)
+	outBuffer := bytes.NewBuffer(nil)
+	errBuffer := bytes.NewBuffer(nil)
 	newTerm := term.NewTerm(
 		os.Stdin,
-		outStream,
-		errStream,
+		// whenever something is written to outBuffer or errBuffer, also write it to the original term's outStream
+		io.MultiWriter(outBuffer, os.Stdout),
+		io.MultiWriter(errBuffer, os.Stderr),
 	)
 	term.DefaultTerm = newTerm
 	defer func() {
 		term.DefaultTerm = originalTerm
 	}()
 	result, err := f()
-	output := outStream.String() + errStream.String()
+	output := outBuffer.String() + errBuffer.String()
 	return output + result, err
 }
