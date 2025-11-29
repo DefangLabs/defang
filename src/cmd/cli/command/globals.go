366f4e17f607accf7f2761107917d4f9a5f116a5
Jordan Stephens <jordan@stephens.io>
2025-11-28 16:34:52 -0800

    def stacks.Load and stacks.Read

--- src/cmd/cli/command/globals.go
+++ src/cmd/cli/command/globals.go
@@ -1,7 +1,6 @@
 package command
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -10,6 +9,7 @@ import (
 	"github.com/DefangLabs/defang/src/pkg/cluster"
 	"github.com/DefangLabs/defang/src/pkg/migrate"
 	"github.com/DefangLabs/defang/src/pkg/modes"
+	"github.com/DefangLabs/defang/src/pkg/stacks"
 	"github.com/DefangLabs/defang/src/pkg/term"
 	"github.com/joho/godotenv"
 	"github.com/spf13/pflag"
@@ -254,17 +254,11 @@ godotenv.Load respects existing environment variables. Stack-specific RC files
 are considered required when specified, while the general RC file is optional.
 */
 func (r *GlobalConfig) loadDotDefang(stackName string) error {
-	dotfile := ".defang"
 	if stackName != "" {
 		// If a stack name is provided, load the stack-specific RC file but return error if it fails or does not exist
-		dotfile = filepath.Join(dotfile, stackName)
-		if abs, err := filepath.Abs(dotfile); err == nil {
-			dotfile = abs
-		}
-		if err := godotenv.Load(dotfile); err != nil {
-			return fmt.Errorf("could not load stack %q: %w", stackName, err)
-		}
+		stacks.Load(stackName) // ensure stack exists
 	} else {
+		dotfile := ".defang"
 		// If no stack name is provided, trying load the general .defang file
 		if abs, err := filepath.Abs(dotfile); err == nil {
 			dotfile = abs
@@ -274,8 +268,8 @@ func (r *GlobalConfig) loadDotDefang(stackName string) error {
 			term.Debugf("could not load stack %q; continuing without env file: %v", stackName, err)
 			return nil // continue if no general env file
 		}
+		term.Debugf("loaded globals from %s", dotfile)
 	}
 
-	term.Debugf("loaded globals from %s", dotfile)
 	return nil
 }
--- src/pkg/stacks/stacks.go
+++ src/pkg/stacks/stacks.go
@@ -2,6 +2,7 @@ package stacks
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -22,7 +23,7 @@ type StackParameters struct {
 
 var validStackName = regexp.MustCompile(`^[a-z][a-z0-9]*$`)
 
-const dotDefang = ".defang"
+const directory = ".defang"
 
 func MakeDefaultName(providerId client.ProviderID, region string) string {
 	compressedRegion := strings.ReplaceAll(region, "-", "")
@@ -42,11 +43,11 @@ func Create(params StackParameters) (string, error) {
 		return "", err
 	}
 
-	if err := os.Mkdir(dotDefang, 0700); err != nil && !errors.Is(err, os.ErrExist) {
+	if err := os.Mkdir(directory, 0700); err != nil && !errors.Is(err, os.ErrExist) {
 		return "", err
 	}
 	filename := filename(params.Name)
-	file, err := os.CreateTemp(dotDefang, params.Name+".tmp.")
+	file, err := os.CreateTemp(directory, params.Name+".tmp.")
 	if err != nil {
 		return "", err
 	}
@@ -76,7 +77,7 @@ type StackListItem struct {
 }
 
 func List() ([]StackListItem, error) {
-	files, err := os.ReadDir(dotDefang)
+	files, err := os.ReadDir(directory)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			return nil, nil
@@ -86,7 +87,7 @@ func List() ([]StackListItem, error) {
 
 	var stacks []StackListItem
 	for _, file := range files {
-		filename := filepath.Join(dotDefang, file.Name())
+		filename := filepath.Join(directory, file.Name())
 		content, err := os.ReadFile(filename)
 		if err != nil {
 			term.Warnf("Skipping unreadable stack file %s: %v\n", filename, err)
@@ -167,5 +168,34 @@ func Remove(name string) error {
 }
 
 func filename(stackname string) string {
-	return filepath.Join(dotDefang, stackname)
+	return filepath.Join(directory, stackname)
+}
+
+func Read(name string) (*StackParameters, error) {
+	path, err := filepath.Abs(filepath.Join(directory, name))
+	if err != nil {
+		return nil, err
+	}
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not read stack %q from %q: %w", name, path, err)
+	}
+	parsed, err := Parse(string(content))
+	if err != nil {
+		return nil, err
+	}
+	return &parsed, nil
+}
+
+func Load(name string) error {
+	path, err := filepath.Abs(filepath.Join(directory, name))
+	if err != nil {
+		return err
+	}
+	if err := godotenv.Load(path); err != nil {
+		return fmt.Errorf("could not load stack %q from %q %w", name, path, err)
+	}
+
+	term.Debugf("loaded globals from %s", path)
+	return nil
 }
