2bb0f1d27b08450d0f2426b0a27df7760e7e6271
Kevin Vo <KevinVo246@hotmail.com>
2025-12-02 15:55:29 -0800

    Update test for stacks and remove default .defangrc

--- src/cmd/cli/command/globals_test.go
+++ src/cmd/cli/command/globals_test.go
@@ -1,25 +1,27 @@
 package command
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 
 	cliClient "github.com/DefangLabs/defang/src/pkg/cli/client"
 	"github.com/DefangLabs/defang/src/pkg/migrate"
 	"github.com/DefangLabs/defang/src/pkg/modes"
 	"github.com/DefangLabs/defang/src/pkg/stacks"
+	"github.com/DefangLabs/defang/src/pkg/term"
 	"github.com/spf13/pflag"
 )
 
 func Test_readGlobals(t *testing.T) {
-	testConfig := GlobalConfig{}
 
 	t.Run("OS env beats any .defang file", func(t *testing.T) {
 		t.Chdir("testdata/with-stack")
 		t.Setenv("VALUE", "from OS env")
-		err := testConfig.loadDotDefang("test")
+		err := loadDotDefang("test")
 		if err != nil {
 			t.Fatalf("%v", err)
 		}
@@ -31,7 +33,7 @@ func Test_readGlobals(t *testing.T) {
 
 	t.Run(".defang/test beats .defang", func(t *testing.T) {
 		t.Chdir("testdata/with-stack")
-		err := testConfig.loadDotDefang("test")
+		err := loadDotDefang("test")
 		if err != nil {
 			t.Fatalf("%v", err)
 		}
@@ -41,20 +43,30 @@ func Test_readGlobals(t *testing.T) {
 		os.Unsetenv("VALUE")
 	})
 
-	t.Run(".defang used if no stack", func(t *testing.T) {
+	t.Run("no stackname provided", func(t *testing.T) {
 		t.Chdir("testdata/no-stack")
-		err := testConfig.loadDotDefang("")
+
+		prevTerm := term.DefaultTerm
+		var stdout, stderr bytes.Buffer
+		term.DefaultTerm = term.NewTerm(os.Stdin, &stdout, &stderr)
+		term.SetDebug(true)
+		t.Cleanup(func() {
+			term.DefaultTerm = prevTerm
+		})
+
+		err := loadDotDefang("")
 		if err != nil {
 			t.Fatalf("%v", err)
 		}
-		if v := os.Getenv("VALUE"); v != "from .defang" {
-			t.Errorf("expected VALUE to be 'from .defang', got '%s'", v)
+
+		expectedDebugMsg := " - No stack name provided; continuing without loading a stack file.\n"
+		if stdout.String() != expectedDebugMsg {
+			t.Errorf("expected debug message %s, got %s", expectedDebugMsg, stdout.String())
 		}
-		os.Unsetenv("VALUE")
 	})
 
 	t.Run("incorrect stackname used if no stack", func(t *testing.T) {
-		err := testConfig.loadDotDefang("non-existent-stack")
+		err := loadDotDefang("non-existent-stack")
 		if err == nil {
 			t.Fatalf("this test should fail for non-existent stack: %v", err)
 		}
@@ -79,36 +91,34 @@ func Test_configurationPrecedence(t *testing.T) {
 			Provider: cliClient.ProviderAuto,
 			Mode:     modes.ModeUnspecified,
 		},
-		Cluster: "",
-		Org:     "",
 	}
 
 	type stack struct {
-		stackname string
-		entries   map[string]string
+		fileName string
+		entries  map[string]string
 	}
 
 	tests := []struct {
-		name         string
-		rcStack      stack
-		createRCFile bool
-		envVars      map[string]string
-		flags        map[string]string
-		expected     GlobalConfig
+		name       string
+		stack      stack
+		createFile bool
+		envVars    map[string]string
+		flags      map[string]string
+		expected   GlobalConfig
 	}{
 		{
-			name:         "Flags override env and env file",
-			createRCFile: true,
-			rcStack: stack{
-				stackname: "test",
+			name:       "Flags override env and env file",
+			createFile: true,
+			stack: stack{
+				fileName: "from-flags",
 				entries: map[string]string{
 					"DEFANG_MODE":            "AFFORDABLE",
 					"DEFANG_VERBOSE":         "false",
 					"DEFANG_DEBUG":           "true",
-					"DEFANG_STACK":           "from-env",
-					"DEFANG_FABRIC":          "from-env-cluster",
+					"DEFANG_STACK":           "from-env-file",
+					"DEFANG_FABRIC":          "from-env-file-cluster",
 					"DEFANG_PROVIDER":        "defang",
-					"DEFANG_ORG":             "from-env-org",
+					"DEFANG_ORG":             "from-env-file-org",
 					"DEFANG_SOURCE_PLATFORM": "heroku",
 					"DEFANG_COLOR":           "never",
 					"DEFANG_TTY":             "false",
@@ -155,18 +165,18 @@ func Test_configurationPrecedence(t *testing.T) {
 			},
 		},
 		{
-			name:         "Env overrides env files when no flags set",
-			createRCFile: true,
-			rcStack: stack{
-				stackname: "test",
+			name:       "Env overrides env files when no flags set",
+			createFile: true,
+			stack: stack{
+				fileName: "from-env",
 				entries: map[string]string{
 					"DEFANG_MODE":            "AFFORDABLE",
 					"DEFANG_VERBOSE":         "false",
 					"DEFANG_DEBUG":           "true",
-					"DEFANG_STACK":           "from-env",
-					"DEFANG_FABRIC":          "from-env-cluster",
+					"DEFANG_STACK":           "from-env-file",
+					"DEFANG_FABRIC":          "from-env-file-cluster",
 					"DEFANG_PROVIDER":        "defang",
-					"DEFANG_ORG":             "from-env-org",
+					"DEFANG_ORG":             "from-env-file-org",
 					"DEFANG_SOURCE_PLATFORM": "heroku",
 					"DEFANG_COLOR":           "never",
 					"DEFANG_TTY":             "false",
@@ -201,18 +211,18 @@ func Test_configurationPrecedence(t *testing.T) {
 			},
 		},
 		{
-			name:         "env file used when no env vars or flags set",
-			createRCFile: true,
-			rcStack: stack{
-				stackname: "test",
+			name:       "env file used when no env vars or flags set",
+			createFile: true,
+			flags:      map[string]string{"stack": "beta"},
+			stack: stack{
+				fileName: "beta",
 				entries: map[string]string{
 					"DEFANG_MODE":            "AFFORDABLE",
 					"DEFANG_VERBOSE":         "true",
 					"DEFANG_DEBUG":           "false",
-					"DEFANG_STACK":           "from-env",
-					"DEFANG_FABRIC":          "from-env-cluster",
+					"DEFANG_FABRIC":          "from-env-file-cluster",
 					"DEFANG_PROVIDER":        "defang",
-					"DEFANG_ORG":             "from-env-org",
+					"DEFANG_ORG":             "from-env-file-org",
 					"DEFANG_SOURCE_PLATFORM": "heroku",
 					"DEFANG_COLOR":           "always",
 					"DEFANG_TTY":             "false",
@@ -223,9 +233,9 @@ func Test_configurationPrecedence(t *testing.T) {
 			expected: GlobalConfig{
 				Verbose:        true,
 				Debug:          false,
-				Stack:          stacks.StackParameters{Name: "from-env", Provider: cliClient.ProviderDefang, Mode: modes.ModeAffordable},
-				Cluster:        "from-env-cluster",
-				Org:            "from-env-org",
+				Stack:          stacks.StackParameters{Name: "beta", Provider: cliClient.ProviderDefang, Mode: modes.ModeAffordable},
+				Cluster:        "from-env-file-cluster",
+				Org:            "from-env-file-org",
 				SourcePlatform: migrate.SourcePlatformHeroku,
 				ColorMode:      ColorAlways,
 				HasTty:         false, // from env
@@ -233,63 +243,48 @@ func Test_configurationPrecedence(t *testing.T) {
 				HideUpdate:     true,  // from env
 			},
 		},
+		// Skipped test case for now; https://github.com/DefangLabs/defang/issues/1686
+		// {
+		// 	name:       "stack env value should be used after loading from env file",
+		// 	createFile: true,
+		// 	stack: stack{
+		// 		fileName: "beta",
+		// 		entries: map[string]string{
+		// 			"DEFANG_STACK": "from-env-file", // this value should be the final output
+		// 		},
+		// 	},
+		// 	expected: GlobalConfig{
+		// 		Stack: stacks.StackParameters{Name: "from-env-file"},
+		// 	},
+		// },
 		{
-			name:         "env file with no values used when no env vars or flags set",
-			createRCFile: true,
-			rcStack: stack{
-				stackname: "test",
+			name:       "env file with no values",
+			createFile: true,
+			flags: map[string]string{
+				"stack": "defang",
 			},
-			expected: defaultConfig,
-		},
-		{
-			name:         "default .defang name, when no env vars or flags",
-			createRCFile: true,
-			rcStack: stack{
-				stackname: "",
-				entries: map[string]string{
-					"DEFANG_MODE":            "AFFORDABLE",
-					"DEFANG_VERBOSE":         "true",
-					"DEFANG_DEBUG":           "false",
-					"DEFANG_STACK":           "from-env",
-					"DEFANG_FABRIC":          "from-env-cluster",
-					"DEFANG_PROVIDER":        "defang",
-					"DEFANG_ORG":             "from-env-org",
-					"DEFANG_SOURCE_PLATFORM": "heroku",
-					"DEFANG_COLOR":           "always",
-					"DEFANG_TTY":             "false",
-					"DEFANG_NON_INTERACTIVE": "true",
-					"DEFANG_HIDE_UPDATE":     "true",
-				},
+			stack: stack{
+				fileName: "defang",
 			},
 			expected: GlobalConfig{
-				Verbose:        true,
-				Debug:          false,
-				Stack:          stacks.StackParameters{Name: "from-env", Provider: cliClient.ProviderDefang, Mode: modes.ModeAffordable},
-				Cluster:        "from-env-cluster",
-				Org:            "from-env-org",
-				SourcePlatform: migrate.SourcePlatformHeroku,
-				ColorMode:      ColorAlways,
-				HasTty:         false, // from env
-				NonInteractive: true,  // from env
-				HideUpdate:     true,  // from env
+				ColorMode:      ColorAuto,
+				HasTty:         true,
+				SourcePlatform: migrate.SourcePlatformUnspecified,
+				Stack: stacks.StackParameters{
+					Name:     "defang",
+					Provider: cliClient.ProviderAuto,
+					Mode:     modes.ModeUnspecified,
+				},
 			},
 		},
 		{
-			name:         "default .defang name and no values, when no env vars or flags",
-			createRCFile: true,
-			rcStack: stack{
-				stackname: "",
-			},
-			expected: defaultConfig,
+			name:       "no env file, no env vars and no flags",
+			createFile: false,
+			expected:   defaultConfig, // should match the initialized defaults above
 		},
 		{
-			name:         "no env file, no env vars and no flags",
-			createRCFile: false,
-			expected:     defaultConfig, // should match the initialized defaults above
-		},
-		{
-			name:         "ignore empty debug bool",
-			createRCFile: false,
+			name:       "ignore empty debug bool",
+			createFile: false,
 			envVars: map[string]string{
 				"DEFANG_DEBUG": "",
 			},
@@ -331,11 +326,11 @@ func Test_configurationPrecedence(t *testing.T) {
 
 			var rcEnvs []string
 			// Create env files in the temporary directory
-			if tt.createRCFile {
+			if tt.createFile {
 				path := filepath.Join(tempDir, ".defang")
-				if tt.rcStack.stackname != "" {
+				if tt.stack.fileName != "" {
 					os.Mkdir(path, 0700)
-					path = filepath.Join(path, tt.rcStack.stackname)
+					path = filepath.Join(path, tt.stack.fileName)
 				}
 
 				f, err := os.Create(path)
@@ -344,7 +339,7 @@ func Test_configurationPrecedence(t *testing.T) {
 				}
 
 				// Write as environment file format
-				for key, value := range tt.rcStack.entries {
+				for key, value := range tt.stack.entries {
 					if _, err := f.WriteString(key + "=" + value + "\n"); err != nil {
 						t.Fatalf("failed to write to file %s: %v", path, err)
 					}
@@ -366,7 +361,7 @@ func Test_configurationPrecedence(t *testing.T) {
 			t.Chdir(tempDir)
 
 			// simulates the actual loading sequence
-			err := testConfig.loadDotDefang(tt.rcStack.stackname)
+			err := loadDotDefang(testConfig.getStackName(flags))
 			if err != nil {
 				t.Fatalf("failed to load env file: %v", err)
 			}
@@ -377,8 +372,8 @@ func Test_configurationPrecedence(t *testing.T) {
 			}
 
 			// verify the final configuration matches expectations
-			if testConfig.Stack.Mode.String() != tt.expected.Stack.Mode.String() {
-				t.Errorf("expected Mode to be '%s', got '%s'", tt.expected.Stack.Mode.String(), testConfig.Stack.Mode.String())
+			if !reflect.DeepEqual(testConfig.Stack, tt.expected.Stack) {
+				t.Errorf("expected Stack to be '%s', got '%s'", tt.expected.Stack, testConfig.Stack)
 			}
 			if testConfig.Verbose != tt.expected.Verbose {
 				t.Errorf("expected Verbose to be %v, got %v", tt.expected.Verbose, testConfig.Verbose)
@@ -386,14 +381,8 @@ func Test_configurationPrecedence(t *testing.T) {
 			if testConfig.Debug != tt.expected.Debug {
 				t.Errorf("expected Debug to be %v, got %v", tt.expected.Debug, testConfig.Debug)
 			}
-			if testConfig.Stack != tt.expected.Stack {
-				t.Errorf("expected Stack to be '%s', got '%s'", tt.expected.Stack, testConfig.Stack)
-			}
 			if testConfig.Cluster != tt.expected.Cluster {
 				t.Errorf("expected Cluster to be '%s', got '%s'", tt.expected.Cluster, testConfig.Cluster)
-			}
-			if testConfig.Stack.Provider != tt.expected.Stack.Provider {
-				t.Errorf("expected Provider to be '%s', got '%s'", tt.expected.Stack.Provider, testConfig.Stack.Provider)
 			}
 			if testConfig.Org != tt.expected.Org {
 				t.Errorf("expected Org to be '%s', got '%s'", tt.expected.Org, testConfig.Org)
