e2cfa548ffca9610b2b8c2386ace1e24ae2d8f0c
Jordan Stephens <jordan@stephens.io>
2025-12-03 13:51:25 -0800

    validate etags on tail

--- src/pkg/cli/client/byoc/gcp/byoc.go
+++ src/pkg/cli/client/byoc/gcp/byoc.go
@@ -478,7 +478,7 @@ func (b *ByocGcp) deploy(ctx context.Context, req *defangv1.DeployRequest, comma
 		return nil, err
 	}
 
-	etag := pkg.RandomID()
+	etag := types.NewEtag()
 	serviceInfos, err := b.GetServiceInfos(ctx, project.Name, req.DelegateDomain, etag, project.Services)
 	if err != nil {
 		return nil, err
