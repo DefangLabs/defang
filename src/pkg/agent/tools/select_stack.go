da1b2108977df1d3fbe2fbaf228d0ca29e8e5d7b
Jordan Stephens <jordan@stephens.io>
2025-12-11 17:56:00 -0800

    SelectStackParams dont need to be str pointers

--- src/pkg/agent/tools/select_stack.go
+++ src/pkg/agent/tools/select_stack.go
@@ -10,15 +10,15 @@ import (
 
 type SelectStackParams struct {
 	common.LoaderParams
-	Stack *string `json:"stack" jsonschema:"description=The name of the stack to use for all tool calls."`
+	Stack string `json:"stack" jsonschema:"description=The name of the stack to use for all tool calls."`
 }
 
 func HandleSelectStackTool(ctx context.Context, params SelectStackParams, sc StackConfig) (string, error) {
 	// User shouldn't need to be require to select a stack
-	if params.Stack != nil {
-		stack, err := stacks.Read(*params.Stack)
+	if params.Stack != "" {
+		stack, err := stacks.Read(params.Stack)
 		if err != nil {
-			return "", fmt.Errorf("Unable to load stack %q, please use the tools awsStackcreate to create a stack for AWS deployment or gcpStackcreate to create a stack for GCP deployment: %w", *params.Stack, err)
+			return "", fmt.Errorf("Unable to load stack %q, please use the tools awsStackcreate to create a stack for AWS deployment or gcpStackcreate to create a stack for GCP deployment: %w", params.Stack, err)
 		}
 
 		sc.Stack = stack
