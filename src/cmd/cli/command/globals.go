a91ec850a40b2474a67734bd950e8d44b89a539d
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2025-12-15 17:58:22 -0400

    Update src/cmd/cli/command/globals.go
    Co-authored-by: Lio李歐 <lionello@users.noreply.github.com>

--- src/cmd/cli/command/globals.go
+++ src/cmd/cli/command/globals.go
@@ -211,8 +211,6 @@ func (r *GlobalConfig) syncFlagsWithEnv(flags *pflag.FlagSet) error {
 		} else if fromEnv, ok := os.LookupEnv("DEFANG_ORG"); ok {
 			r.Tenant = fromEnv
 			term.Warn("DEFANG_ORG is deprecated; use DEFANG_WORKSPACE instead")
-		} else if _, ok := os.LookupEnv("DEFANG_TENANT"); ok {
-			term.Warn("DEFANG_TENANT is no longer supported; use DEFANG_WORKSPACE instead")
 		}
 	}
 
