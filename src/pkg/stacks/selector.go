e5cdb15cd0c7a9cf599159f8287a344482ad076e
Jordan Stephens <jordan@stephens.io>
2025-12-15 18:58:46 -0800

    print beta stack info message

--- src/pkg/stacks/selector.go
+++ src/pkg/stacks/selector.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
 
 	"github.com/DefangLabs/defang/src/pkg/elicitations"
+	"github.com/DefangLabs/defang/src/pkg/term"
 )
 
 const CreateNewStack = "Create new stack"
@@ -64,10 +68,27 @@ func (ss *stackSelector) elicitStackSelection(ctx context.Context, ec elicitatio
 	}
 	stackNames = append(stackNames, CreateNewStack)
 
+	printStacksInfoMessage(stackNames)
 	selectedStackName, err := ec.RequestEnum(ctx, "Select a stack", "stack", stackNames)
 	if err != nil {
 		return "", fmt.Errorf("failed to elicit stack choice: %w", err)
 	}
 
 	return selectedStackName, nil
+}
+
+func printStacksInfoMessage(stacks []string) {
+	// If there is a stack named "beta", print an informational message about it
+	betaExists := slices.Contains(stacks, "beta")
+	if betaExists {
+		infoLine := "This project was deployed with an implicit Stack called 'beta' before Stacks were introduced."
+		if len(stacks) == 1 {
+			infoLine += "\n   To update your existing deployment, select the 'beta' Stack.\n" +
+				"Creating a new Stack will result in a separate deployment instance."
+		}
+		infoLine += "\n   To learn more about Stacks, visit: https://docs.defang.io/docs/concepts/stacks"
+		term.Info(infoLine + "\n")
+	}
+	executable, _ := os.Executable()
+	term.Infof("To skip this prompt, run %s up --stack=%s", filepath.Base(executable), "<stack_name>")
 }
