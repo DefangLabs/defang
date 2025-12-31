dfbb21863c89b777e114490c24f601ee7faa0992
Kevin Vo <KevinVo246@hotmail.com>
2025-12-30 16:52:14 -0800

    simplify test

--- src/pkg/cli/compose/baseimage_test.go
+++ src/pkg/cli/compose/baseimage_test.go
@@ -1,35 +1,27 @@
 package compose
 
 import (
+	"slices"
 	"testing"
 )
 
 func TestFindAllBaseImages(t *testing.T) {
-	t.Run("TestFindAllBaseImages", func(t *testing.T) {
+	baseimageComposePath := "../../../testdata/base-image/compose.yaml"
 
-		baseimageComposePath := "../../../testdata/base-image/compose.yaml"
+	// Load the actual base-image compose.yaml file
+	loader := NewLoader(WithPath(baseimageComposePath))
+	project, err := loader.LoadProject(t.Context())
+	if err != nil {
+		t.Fatalf("Failed to load base-image compose.yaml: %v", err)
+	}
 
-		// Load the actual base-image compose.yaml file
-		loader := NewLoader(WithPath(baseimageComposePath))
-		project, err := loader.LoadProject(t.Context())
-		if err != nil {
-			t.Fatalf("Failed to load base-image compose.yaml: %v", err)
-		}
+	images, err := FindAllBaseImages(project)
+	if err != nil {
+		t.Fatalf("Received unexpected error: %v", err)
+	}
 
-		images, err := FindAllBaseImages(project)
-		if err != nil {
-			t.Fatalf("Received unexpected error: %v", err)
-		}
-
-		expectedImages := []string{"alpine", "alpine:latest", "ubuntu"}
-		if len(images) != len(expectedImages) {
-			t.Fatalf("Expected %d images, got %d", len(expectedImages), len(images))
-		}
-
-		for i, img := range expectedImages {
-			if images[i] != img {
-				t.Errorf("Expected image %s, got %s", img, images[i])
-			}
-		}
-	})
+	expectedImages := []string{"alpine", "alpine:latest", "ubuntu"}
+	if !slices.Equal(images, expectedImages) {
+		t.Errorf("Expected images %v, got %v", expectedImages, images)
+	}
 }
