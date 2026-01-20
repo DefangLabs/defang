6e3717d2a0ccde0b4370e5ee212426c33c0b2a3d
Lio李歐 <lionello@users.noreply.github.com>
2026-01-19 16:46:39 -0800

    feat: allow multiple configs for --env, --random, and --env-file (#1805)
    * feat: allow multiple configs for --env, --random, and --env-file
    
    * Mock PutSecret implementation in tests
    
    Fixes:
    got "unimplemented: io.defang.v1.FabricController.PutSecret is not
    implemented\nunimplemented: io.defang.v1.FabricController.PutSecret is not implemented"
    
    * Add more test and reset flags between tests
    
    * dry run config set
    
    * Update src/cmd/cli/command/config_test.go
    
    * more config testcases
    
    * add error when mutiple configs do not have '='
    
    * enhance config tests with additional scenarios for env and env-file flags
    
    ---------
    
    Co-authored-by: Kevin Vo <KevinVo246@hotmail.com>

--- src/pkg/cli/configSet.go
+++ src/pkg/cli/configSet.go
@@ -2,15 +2,29 @@ package cli
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/DefangLabs/defang/src/pkg"
 	"github.com/DefangLabs/defang/src/pkg/cli/client"
 	"github.com/DefangLabs/defang/src/pkg/dryrun"
 	"github.com/DefangLabs/defang/src/pkg/term"
 	defangv1 "github.com/DefangLabs/defang/src/protos/io/defang/v1"
 )
 
+type ErrInvalidConfigName struct {
+	Name string
+}
+
+func (e ErrInvalidConfigName) Error() string {
+	return fmt.Sprintf("invalid config name; must be alphanumeric or _, cannot start with a number: %q", e.Name)
+}
+
 func ConfigSet(ctx context.Context, projectName string, provider client.Provider, name string, value string) error {
 	term.Debugf("Setting config %q in project %q", name, projectName)
+
+	if !pkg.IsValidSecretName(name) {
+		return ErrInvalidConfigName{Name: name}
+	}
 
 	if dryrun.DoDryRun {
 		return dryrun.ErrDryRun
