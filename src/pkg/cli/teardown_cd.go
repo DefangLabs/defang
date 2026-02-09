5331e5d195f4fe5b7334e34f9ff54d9586ed578c
jordanstephens <jordan@stephens.io>
2026-02-09 13:12:34 -0800

    improve cd teardown error handling
    * only print suggestion to use `--force` if there are existing projects
    * separate hint from error
    * print the names of the existing projects and stacks

--- src/pkg/cli/teardown_cd.go
+++ src/pkg/cli/teardown_cd.go
@@ -10,19 +10,30 @@ import (
 	"github.com/DefangLabs/defang/src/pkg/term"
 )
 
+var ErrExistingProjects = errors.New("there are still deployed projects")
+
 func TearDownCD(ctx context.Context, provider client.Provider, force bool) error {
 	if dryrun.DoDryRun {
 		return errors.New("dry run")
 	}
 	if !force {
-		if list, err := provider.CdList(ctx, false); err != nil {
-			return fmt.Errorf("could not get list of services; use --force to tear down anyway: %w", err)
-		} else {
-			for range list {
-				return errors.New("there are still deployed services; use --force to tear down anyway")
+		list, err := provider.CdList(ctx, false)
+		if err != nil {
+			return fmt.Errorf("could not get list of projects: %w", err)
+		}
+
+		found := false
+		for project := range list {
+			if !found {
+				term.Info("There are still deployed projects:")
 			}
+			fmt.Println(project)
+			found = true
+		}
+		if found {
+			return ErrExistingProjects
 		}
 	}
-	term.Warn("Deleting the CD cluster; this does not delete services or configs!")
+	term.Warn("Deleting the CD cluster; this does not delete projects or configs!")
 	return provider.TearDownCD(ctx)
 }
