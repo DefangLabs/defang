4f9f2e8d0c3bba50e5cd12d5ecafbee54e391daa
jordanstephens <jordan@stephens.io>
2026-02-10 13:36:32 -0800

    remove remote project name fallback

--- src/pkg/cli/client/projectName.go
+++ src/pkg/cli/client/projectName.go
@@ -2,26 +2,16 @@ package client
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/DefangLabs/defang/src/pkg/term"
 )
 
 // Deprecated: should use stacks instead of ProjectName fallback.
 func LoadProjectNameWithFallback(ctx context.Context, loader Loader, provider Provider) (string, error) {
-	var loadErr error
-	if loader != nil {
-		projectName, _, err := loader.LoadProjectName(ctx)
-		if err == nil {
-			return projectName, nil
-		}
-		term.Debug("Failed to load local project:", err)
-		loadErr = err
-	}
-	term.Debug("Trying to get the remote project name from the provider")
-	projectName, err := provider.RemoteProjectName(ctx)
+	projectName, _, err := loader.LoadProjectName(ctx)
 	if err != nil {
-		return "", fmt.Errorf("%w and %w", loadErr, err)
+		term.Debug("Failed to load local project:", err)
+		return "", err
 	}
 	return projectName, nil
 }
