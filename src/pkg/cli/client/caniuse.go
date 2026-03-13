14575c458c424850fc35ae7c3f21f77e7fc1b850
Lionello Lunesu <lio+git@lunesu.com>
2026-03-12 18:45:42 -0700

    feat: add CD and Pulumi version fields to CanIUseRequest and update logic for version resolution

--- src/pkg/cli/client/caniuse.go
+++ src/pkg/cli/client/caniuse.go
@@ -14,46 +14,48 @@ func CanIUseProvider(ctx context.Context, client FabricClient, provider Provider
 		return err
 	}
 
-	canUseReq := defangv1.CanIUseRequest{
+	cdOverride := os.Getenv("DEFANG_CD_IMAGE")
+	pulumiOverride := os.Getenv("DEFANG_PULUMI_VERSION")
+
+	// Look up previously deployed versions to send to fabric and for client-side pinning
+	var prevCD, prevPulumi string
+	if projectName != "" && !allowUpgrade && (cdOverride == "" || pulumiOverride == "") {
+		if pu, err := provider.GetProjectUpdate(ctx, projectName); err != nil || pu == nil {
+			term.Debugf("unable to get project update for %q: %v", projectName, err)
+		} else {
+			prevCD = pu.CdVersion
+			prevPulumi = pu.PulumiVersion
+		}
+	}
+
+	resp, err := client.CanIUse(ctx, &defangv1.CanIUseRequest{
 		Project:           projectName,
 		Provider:          info.Provider.Value(),
 		ProviderAccountId: info.AccountID,
 		Region:            info.Region,
 		ServiceCount:      int32(serviceCount), // #nosec G115 - service count will not overflow int32
 		Stack:             provider.GetStackName(),
-	}
-
-	resp, err := client.CanIUse(ctx, &canUseReq)
+		CdVersion:         prevCD,
+		PulumiVersion:     prevPulumi,
+	})
 	if err != nil {
 		return err
 	}
 
-	// Hard override from env vars takes absolute precedence
-	cdOverride := os.Getenv("DEFANG_CD_IMAGE")
-	if cdOverride != "" {
-		resp.CdImage = cdOverride
-	}
-	pulumiOverride := os.Getenv("DEFANG_PULUMI_VERSION")
-	if pulumiOverride != "" {
-		resp.PulumiVersion = pulumiOverride
-	}
-
-	// Version pinning: use previous versions unless explicitly upgrading or overridden by env
-	if projectName != "" && (cdOverride == "" || pulumiOverride == "") {
-		if projUpdate, err := provider.GetProjectUpdate(ctx, projectName); err != nil || projUpdate == nil {
-			term.Debugf("unable to get project update for %q: %v", projectName, err)
-		} else {
-			if cdOverride == "" {
-				resp.CdImage = pinVersion(resp.CdImage, projUpdate.CdVersion, "CD image", allowUpgrade)
-			}
-			if pulumiOverride == "" {
-				resp.PulumiVersion = pinVersion(resp.PulumiVersion, projUpdate.PulumiVersion, "Pulumi version", allowUpgrade)
-			}
-		}
-	}
+	// Resolve each version: env override > client-side pinning > fabric response
+	resp.CdImage = resolveVersion(cdOverride, resp.CdImage, prevCD, "CD image", allowUpgrade)
+	resp.PulumiVersion = resolveVersion(pulumiOverride, resp.PulumiVersion, prevPulumi, "Pulumi version", allowUpgrade)
 
 	provider.SetCanIUseConfig(resp)
 	return nil
+}
+
+// resolveVersion picks the version to use given an env override, the fabric's response, and the previously deployed version.
+func resolveVersion(envOverride, latest, previous, label string, allowUpgrade bool) string {
+	if envOverride != "" {
+		return envOverride
+	}
+	return pinVersion(latest, previous, label, allowUpgrade)
 }
 
 func pinVersion(latest, previous, label string, allowUpgrade bool) string {
--- src/protos/io/defang/v1/fabric.pb.go
+++ src/protos/io/defang/v1/fabric.pb.go
@@ -1845,6 +1845,8 @@ type CanIUseRequest struct {
 	Stack             string                 `protobuf:"bytes,4,opt,name=stack,proto3" json:"stack,omitempty"`
 	Region            string                 `protobuf:"bytes,5,opt,name=region,proto3" json:"region,omitempty"`
 	ProviderAccountId string                 `protobuf:"bytes,6,opt,name=provider_account_id,json=providerAccountId,proto3" json:"provider_account_id,omitempty"`
+	CdVersion         string                 `protobuf:"bytes,7,opt,name=cd_version,json=cdVersion,proto3" json:"cd_version,omitempty"`             // currently deployed CD image; empty for new projects or when --allow-upgrade is set
+	PulumiVersion     string                 `protobuf:"bytes,8,opt,name=pulumi_version,json=pulumiVersion,proto3" json:"pulumi_version,omitempty"` // currently deployed Pulumi version; empty for new projects or when --allow-upgrade is set
 	unknownFields     protoimpl.UnknownFields
 	sizeCache         protoimpl.SizeCache
 }
@@ -1917,6 +1919,20 @@ func (x *CanIUseRequest) GetRegion() string {
 func (x *CanIUseRequest) GetProviderAccountId() string {
 	if x != nil {
 		return x.ProviderAccountId
+	}
+	return ""
+}
+
+func (x *CanIUseRequest) GetCdVersion() string {
+	if x != nil {
+		return x.CdVersion
+	}
+	return ""
+}
+
+func (x *CanIUseRequest) GetPulumiVersion() string {
+	if x != nil {
+		return x.PulumiVersion
 	}
 	return ""
 }
@@ -5706,14 +5722,17 @@ const file_io_defang_v1_fabric_proto_rawDesc = "" +
 	"\x04arch\x18\x05 \x01(\tR\x04arch\x1a=\n" +
 	"\x0fPropertiesEntry\x12\x10\n" +
 	"\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n" +
-	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01\"\xe1\x01\n" +
+	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01\"\xa7\x02\n" +
 	"\x0eCanIUseRequest\x12\x18\n" +
 	"\aproject\x18\x01 \x01(\tR\aproject\x122\n" +
 	"\bprovider\x18\x02 \x01(\x0e2\x16.io.defang.v1.ProviderR\bprovider\x12#\n" +
 	"\rservice_count\x18\x03 \x01(\x05R\fserviceCount\x12\x14\n" +
 	"\x05stack\x18\x04 \x01(\tR\x05stack\x12\x16\n" +
 	"\x06region\x18\x05 \x01(\tR\x06region\x12.\n" +
-	"\x13provider_account_id\x18\x06 \x01(\tR\x11providerAccountId\"\xae\x01\n" +
+	"\x13provider_account_id\x18\x06 \x01(\tR\x11providerAccountId\x12\x1d\n" +
+	"\n" +
+	"cd_version\x18\a \x01(\tR\tcdVersion\x12%\n" +
+	"\x0epulumi_version\x18\b \x01(\tR\rpulumiVersion\"\xae\x01\n" +
 	"\x0fCanIUseResponse\x12\x19\n" +
 	"\bcd_image\x18\x02 \x01(\tR\acdImage\x12\x10\n" +
 	"\x03gpu\x18\x03 \x01(\bR\x03gpu\x12#\n" +
