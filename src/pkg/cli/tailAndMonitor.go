49bb1016df5249f682282d9b954079e6077c8562
Jordan Stephens <jordan@stephens.io>
2026-04-07 14:21:22 -0700

    Apply suggestions from code review
    Co-authored-by: Lio李歐 <lionello@users.noreply.github.com>

--- src/pkg/cli/tailAndMonitor.go
+++ src/pkg/cli/tailAndMonitor.go
@@ -55,9 +55,9 @@ func TailAndMonitor(ctx context.Context, project *compose.Project, provider clie
 			cdErr = err
 			// When CD fails, stop WaitServiceState
 			cancelSvcStatus(cdErr)
+		} else {
+				term.Info("Deployment complete. Waiting for services to be healthy...")
 		}
-
-		term.Info("Deployment complete. Waiting for services to be healthy...")
 	}()
 
 	errMonitoringDone := errors.New("monitoring done") // pseudo error to signal that monitoring is done
