17b1da3f293201ac1be1273ed29a8b7eb44de095
Edward J <edw@defang.io>
2026-04-07 22:21:04 -0700

    Update nix sha

--- pkgs/defang/cli.nix
+++ pkgs/defang/cli.nix
@@ -7,7 +7,7 @@ buildGo124Module {
   pname = "defang-cli";
   version = "git";
   src = lib.cleanSource ../../src;
-  vendorHash = "sha256-/cHLeXdprRCbbFT8VV99Y+ojMst0NELfajEyMPbuL50=";
+  vendorHash = "sha256-I1FzA9H2VZSY8B/2AXbhrji+BxnHxN0wMd6hzJ9rm1c=";
 
   subPackages = [ "cmd/cli" ];
 
