23cabe58aa7cae2d3618c3da0566c8aa08083383
Lio李歐 <lionello@users.noreply.github.com>
2026-07-20 16:26:27 +0200

    Apply suggestion from @lionello

--- src/pkg/cli/compose/fixup.go
+++ src/pkg/cli/compose/fixup.go
@@ -248,9 +248,9 @@ func parsePortString(port string) (uint32, error) {
 }
 
 const (
-	liteLLMPort         uint32 = 4000
-	defaultLLMCPUs             = 0.5
-	defaultLLMMemoryMiB        = 1024 // 512 MiB OOMed during AWS E2E; use the next size up.
+	liteLLMPort         = 4000
+	defaultLLMCPUs      = 0.5
+	defaultLLMMemoryMiB = 1024 // 512 MiB OOMed during AWS E2E; use the next size up.
 )
 
 func fixupLLM(svccfg *composeTypes.ServiceConfig) {
